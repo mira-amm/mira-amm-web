fc050b70ab73413a8c273fcc07fcabda7c15eb09
Sarmat <skhachirov12@gmail.com>
2024-07-15 13:39:35 +0300

    wip

--- src/components/common/Swap/Swap.tsx
+++ src/components/common/Swap/Swap.tsx
@@ -1,7 +1,10 @@
-import {useMemo, useState} from "react";
-import {useConnectUI, useIsConnected, useWallet} from "@fuels/react";
+import { useMemo, useState } from "react";
+import { useConnectUI, useIsConnected, useWallet } from "@fuels/react";
 import MiraAmm from "sdk-ts";
-import { AssetIdInput, AssetInput } from "sdk-ts/dist/typegen/amm-contract/AmmContractAbi";
+import {
+  AssetIdInput,
+  AssetInput,
+} from "sdk-ts/dist/typegen/amm-contract/AmmContractAbi";
 
 import CurrencyBox from "@/src/components/common/Swap/components/CurrencyBox/CurrencyBox";
 import SettingsIcon from "@/src/components/icons/Settings/SettingsIcon";
@@ -10,19 +13,20 @@ import ConvertIcon from "@/src/components/icons/Convert/ConvertIcon";
 import IconButton from "@/src/components/common/IconButton/IconButton";
 import useModal from "@/src/hooks/useModal/useModal";
 import InfoIcon from "@/src/components/icons/Info/InfoIcon";
-import {coinsConfig} from "@/src/utils/coinsConfig";
+import { coinsConfig } from "@/src/utils/coinsConfig";
 
 import styles from "./Swap.module.css";
+import { BN } from "fuels";
 
-export type CurrencyBoxMode = 'buy' | 'sell';
+export type CurrencyBoxMode = "buy" | "sell";
 // TODO: Store amount later + specify coins dict rather than string
 type SwapState = Record<CurrencyBoxMode, string>;
 
 const Swap = () => {
   const [Modal, openModal, closeModal] = useModal();
   const [coins, setCoins] = useState<SwapState>({
-    sell: '',
-    buy: ''
+    sell: "",
+    buy: "",
   });
 
   const { isConnected } = useIsConnected();
@@ -33,18 +37,26 @@ const Swap = () => {
   const miraInstance = useMemo(() => {
     if (wallet && wallet.provider) {
       // @ts-ignore
-      return new MiraAmm({ wallet, provider: wallet?.provider, contractAddress: '0xb942cd8440a4fe2e2e7548cfcb1d1547881cfe02db66a463b19e1e46ae56f0ca' });
+      return new MiraAmm({
+        wallet,
+        provider: wallet?.provider,
+        contractAddress:
+          "0xb942cd8440a4fe2e2e7548cfcb1d1547881cfe02db66a463b19e1e46ae56f0ca",
+      });
     }
   }, [wallet]);
 
-  const selectCoin = (mode: 'buy' | 'sell') => {
+  const selectCoin = (mode: "buy" | "sell") => {
     return (coin: string) => {
-      if ((mode === 'buy' && coins.sell === coin) || (mode === 'sell' && coins.buy === coin)) {
+      if (
+        (mode === "buy" && coins.sell === coin) ||
+        (mode === "sell" && coins.buy === coin)
+      ) {
         changeCoins();
       } else {
-        setCoins({...coins, [mode]: coin});
+        setCoins({ ...coins, [mode]: coin });
       }
-    }
+    };
   };
 
   const changeCoins = () => {
@@ -56,26 +68,30 @@ const Swap = () => {
     const sellAssetIdInput: AssetIdInput = { bits: sellAssetId };
     const buyAssetId = coinsConfig.get(coins.buy)?.assetId!;
     const buyAssetIdInput: AssetIdInput = { bits: buyAssetId };
-    const assetPair: [AssetIdInput, AssetIdInput] = [sellAssetIdInput, buyAssetIdInput];
-    const assetSwapInput:AssetInput = {id: sellAssetIdInput, amount: 100};
+    const assetPair: [AssetIdInput, AssetIdInput] = [
+      sellAssetIdInput,
+      buyAssetIdInput,
+    ];
+    const assetSwapInput: AssetInput = { id: sellAssetIdInput, amount: 100 };
     if (miraInstance) {
       // Preview swap exact input
       // const result = await miraInstance.previewSwapExactInput(assetPair, assetSwapInput, { gasLimit: 100_000, maxFee: 100_000 });
 
       // Preview swap exact output
       // const result = await miraInstance.previewSwapExactOutput(assetPair, assetSwapInput, { gasLimit: 100_000, maxFee: 100_000 });
-      
-      // Balance 
+
+      // Balance
       // const result = await miraInstance.balance(assetPair, sellAssetIdInput, { gasLimit: 100_000, maxFee: 100_000 });
 
       // Preview add liquidity
       // const result = await miraInstance.previewAddLiquidity(assetPair, assetSwapInput, { gasLimit: 100_000, maxFee: 100_000 });
-      
-      const result = await miraInstance.previewAddLiquidity(assetPair, assetSwapInput, { gasLimit: 100_000, maxFee: 100_000 });
 
-      console.log(result.value);
+      const result = await miraInstance.swapExactInput( assetPair, assetSwapInput.amount,0,1000000000, { gasLimit: 100_000, maxFee: 100_000 });
+      console.log(result);
+      const res = await wallet?.sendTransaction(result); 
+      console.log(res);
     }
-  }
+  };
 
   return (
     <>
@@ -86,15 +102,27 @@ const Swap = () => {
             <SettingsIcon />
           </IconButton>
         </div>
-        <CurrencyBox mode="sell" selectedCoin={coins.sell} selectCoin={selectCoin('sell')} />
+        <CurrencyBox
+          mode="sell"
+          selectedCoin={coins.sell}
+          selectCoin={selectCoin("sell")}
+        />
         <div className={styles.splitter}>
           <IconButton onClick={changeCoins} className={styles.convertButton}>
             <ConvertIcon />
           </IconButton>
         </div>
-        <CurrencyBox mode="buy" selectedCoin={coins.buy} selectCoin={selectCoin('buy')}/>
+        <CurrencyBox
+          mode="buy"
+          selectedCoin={coins.buy}
+          selectCoin={selectCoin("buy")}
+        />
         {!isConnected && (
-          <ActionButton variant="secondary" onClick={connect} loading={isConnecting}>
+          <ActionButton
+            variant="secondary"
+            onClick={connect}
+            loading={isConnecting}
+          >
             Connect Wallet
           </ActionButton>
         )}
@@ -110,27 +138,25 @@ const Swap = () => {
           <div className={styles.settingsSection}>
             <p>Slippage Tolerance</p>
             <p className={styles.settingsText}>
-              The amount the price can change unfavorably before the trade reverts
+              The amount the price can change unfavorably before the trade
+              reverts
             </p>
           </div>
           <div className={styles.settingsSection}>
             <div className={styles.slippageButtons}>
-              <button className={styles.slippageButton}>
-                Auto
-              </button>
-              <button className={styles.slippageButton}>
-                Custom
-              </button>
+              <button className={styles.slippageButton}>Auto</button>
+              <button className={styles.slippageButton}>Custom</button>
             </div>
             <input type="text" className={styles.slippageInput} value="1%" />
           </div>
           <div className={styles.settingsSection}>
             <p className={styles.infoHeading}>
-              <InfoIcon/>
+              <InfoIcon />
               Pay attention
             </p>
             <p className={styles.settingsText}>
-              Customized price impact limit may lead to loss of funds. Use it at your own risk
+              Customized price impact limit may lead to loss of funds. Use it at
+              your own risk
             </p>
           </div>
         </div>
