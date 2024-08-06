3a0735797ae41b63024973172d0443ecb01be71f
Michael Poplavkov <mihail.poplavkov@gmail.com>
2024-08-06 18:34:23 +0100

    Add remaining connectors (#8)
    * Add remaining connectors
    
    * Change url

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # MIRA AMM Web app
 
 [production](https://mira.ly/)
+
 [development](https://d2gdnex5lpr7h7.cloudfront.net)
--- src/core/providers/Providers.tsx
+++ src/core/providers/Providers.tsx
@@ -3,23 +3,58 @@
 import {ReactNode} from "react";
 import {FuelProvider} from "@fuels/react";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {BurnerWalletConnector, FueletWalletConnector, FuelWalletConnector} from "@fuels/connectors";
+import {
+  BakoSafeConnector,
+  BurnerWalletConnector,
+  FueletWalletConnector,
+  FuelWalletConnector,
+  SolanaConnector,
+  WalletConnectConnector
+} from "@fuels/connectors";
+import {createConfig, http, injected} from "@wagmi/core";
 import {FuelConfig, FuelConnector} from "fuels";
 import PersistentConnectorProvider from "@/src/core/providers/PersistentConnector";
+import {sepolia} from "@wagmi/core/chains";
+import {walletConnect} from "@wagmi/connectors";
 
 type Props = {
   children: ReactNode;
 };
 
+const WalletConnectProjectId = '35b967d8f17700b2de24f0abee77e579';
 const queryClient = new QueryClient();
+const wagmiConfig = createConfig({
+  chains: [sepolia],
+  transports: {
+    [sepolia.id]: http(),
+  },
+  connectors: [
+    injected({shimDisconnect: false}),
+    walletConnect({
+      projectId: WalletConnectProjectId,
+      metadata: {
+        name: "Mira DEX",
+        description: "The Liquidity Hub on Fuel",
+        url: "https://mira.ly/",
+        icons: ["https://connectors.fuel.network/logo_white.png"],
+      },
+    }),
+  ],
+});
 
 const Providers = ({children}: Props) => {
   let connectors: FuelConnector[] = [];
   if (typeof window !== 'undefined') {
     connectors = [
       new FueletWalletConnector(),
-      new FuelWalletConnector(),
       new BurnerWalletConnector(),
+      new FuelWalletConnector(),
+      new BakoSafeConnector(),
+      new WalletConnectConnector({
+        projectId: WalletConnectProjectId,
+        wagmiConfig: wagmiConfig as any
+      }),
+      new SolanaConnector({projectId: WalletConnectProjectId})
     ];
   }
 
