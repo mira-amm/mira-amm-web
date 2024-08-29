839aaa5d7914e06b794116f7cf1064e830778d99
Nikita Chistousov <svvald@icloud.com>
2024-08-29 23:01:27 +0200

    feat: update seo titles and descriptions for mira pages (#14)

--- app/faucet/layout.tsx
+++ app/faucet/layout.tsx
@@ -6,7 +6,19 @@ type Props = Readonly<{
 }>;
 
 export const metadata: Metadata = {
-  title: 'Fuel Test Sepolia ETH Faucet | MIRA DEX',
+  title: 'Provide Liquidity on MIRA AMM | Earn Rewards with MIRA Liquidity Pools',
+  description: "Contribute to liquidity pools on MIRA AMM. Get rewards for providing liquidity using MIRA protocol. Maximize your earnings with our incentive points program. Join our liquidity providers",
+  openGraph: {
+    title: "Swap on MIRA DEX instantly with low slippage | MIRA Swaps",
+    url: 'https://mira.ly/faucet',
+    description: "Contribute to liquidity pools on MIRA AMM. Get rewards for providing liquidity using MIRA protocol. Maximize your earnings with our incentive points program. Join our liquidity providers",
+    images: 'https://mira.ly/images/preview.png',
+  },
+  twitter: {
+    title: "Swap on MIRA DEX instantly with low slippage | MIRA Swaps",
+    description: "Contribute to liquidity pools on MIRA AMM. Get rewards for providing liquidity using MIRA protocol. Maximize your earnings with our incentive points program. Join our liquidity providers",
+    images: 'https://mira.ly/images/preview.png',
+  },
 };
 
 const FaucetLayout = ({ children }: Props) => {
