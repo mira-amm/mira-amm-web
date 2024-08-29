513955a924b3cb0cdc7b8ca1c1ec90c44a3ae4ff
Nikita Chistousov <nikita_chistousov@epam.com>
2024-08-29 13:10:22 +0200

    feat: update seo titles and descriptions for mira pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,21 +52,21 @@ const sfCompactText = localFont({
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://mira.ly/'),
-  title: "Mira DEX - Trade, Swap and manage liquidity on the Fuel blockchain with MIRA AMM",
-  description: "Discover the fastest AMM on Fuel blockchain. Trade securely with low fees and slippage, swap and earn by providing liquidity. Join the future of decentralized finance with MIRA Protocol.",
+  title: "MIRA DEX: Swap, Trade, and Earn on Fuel Blockchain | MIRA Exchange & AMM",
+  description: "Join MIRA DEX, the leading AMM on Fuel Blockchain. Experience rapid, low-fee trading and swaps. Dive into our decentralized platform to manage liquidity and maximize earnings. Trade securely, with minimal slippage. Your gateway to decentralized trading",
   icons: {
     icon: "/favicon.png",
   },
   openGraph: {
-    title: "Mira DEX - Trade, Swap and manage liquidity on the Fuel blockchain with MIRA AMM",
+    title: "MIRA DEX: Swap, Trade, and Earn on Fuel Blockchain | MIRA Exchange & AMM",
     siteName: 'Mira Automated Market Maker',
     url: 'https://mira.ly/',
-    description: "Discover the fastest AMM on Fuel blockchain. Trade securely with low fees and slippage, swap and earn by providing liquidity. Join the future of decentralized finance with MIRA Protocol.",
+    description: "Join MIRA DEX, the leading AMM on Fuel Blockchain. Experience rapid, low-fee trading and swaps. Dive into our decentralized platform to manage liquidity and maximize earnings. Trade securely, with minimal slippage. Your gateway to decentralized trading",
     images: 'https://mira.ly/images/preview.png',
   },
   twitter: {
-    title: "Mira DEX - Trade, Swap and manage liquidity on the Fuel blockchain with MIRA AMM",
-    description: "Discover the fastest AMM on Fuel blockchain. Trade securely with low fees and slippage, swap and earn by providing liquidity. Join the future of decentralized finance with MIRA Protocol.",
+    title: "MIRA DEX: Swap, Trade, and Earn on Fuel Blockchain | MIRA Exchange & AMM",
+    description: "Join MIRA DEX, the leading AMM on Fuel Blockchain. Experience rapid, low-fee trading and swaps. Dive into our decentralized platform to manage liquidity and maximize earnings. Trade securely, with minimal slippage. Your gateway to decentralized trading",
     images: 'https://mira.ly/images/preview.png',
   },
 };
