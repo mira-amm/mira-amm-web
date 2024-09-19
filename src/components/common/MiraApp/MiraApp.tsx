7ed9e85db4877614dbf30e228c4a7ea2806558f5
Voiislav Mitrovich <voiislav@MacBook-Air-Voiislav.local>
2024-09-19 21:55:14 +0200

    fix: mira logo

--- src/components/common/MiraApp/MiraApp.tsx
+++ src/components/common/MiraApp/MiraApp.tsx
@@ -1,11 +1,13 @@
 import styles from "./MiraApp.module.css";
 import LaunchAppButton from "../LaunchAppButton/LaunchAppButton";
-import MiraLogo from "../../icons/MiraLogo/MiraLogo";
+// import MiraLogo from "../../icons/MiraLogo/MiraLogo";
+import miraLogo from "../../icons/26.png";
+import Image from "next/image";
 
 export const MiraApp = () => {
   return (
     <div className={styles.mira}>
-      <div className={styles.miraLogo}><MiraLogo /></div>
+      <img className={styles.logo} src={miraLogo.src} alt="Mira Logo"/>
       <h2 className={styles.title}>Welcome to MIRA</h2>
       <p className={styles.description}>
         Exceptional capital efficiency with robust liquidity and minimal fees
