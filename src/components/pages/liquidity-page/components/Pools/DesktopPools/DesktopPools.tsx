7364afe6cc93e17b0ee555c09116be27bfc08b6b
Voiislav Mitrovich <voiislav@MacBook-Air-Voiislav.local>
2024-09-22 13:32:52 +0200

    feat: add create pool button to the liquidity page

--- src/components/pages/liquidity-page/components/Pools/DesktopPools/DesktopPools.tsx
+++ src/components/pages/liquidity-page/components/Pools/DesktopPools/DesktopPools.tsx
@@ -1,15 +1,15 @@
 import CoinPair from "@/src/components/common/CoinPair/CoinPair";
 import ActionButton from "@/src/components/common/ActionButton/ActionButton";
 
-import styles from './DesktopPools.module.css';
-import {useRouter} from "next/navigation";
-import {createPoolKey, getCoinByAssetId} from "@/src/utils/common";
-import {PoolMetadata} from "mira-dex-ts";
-import {clsx} from "clsx";
+import styles from "./DesktopPools.module.css";
+import { useRouter } from "next/navigation";
+import { createPoolKey, getCoinByAssetId } from "@/src/utils/common";
+import { PoolMetadata } from "mira-dex-ts";
+import { clsx } from "clsx";
 
 type Props = {
   poolsData: (PoolMetadata | null | undefined)[] | undefined;
-}
+};
 
 const DesktopPools = ({ poolsData }: Props) => {
   const router = useRouter();
@@ -23,51 +23,57 @@ const DesktopPools = ({ poolsData }: Props) => {
   };
 
   return (
-    <table className={clsx(styles.desktopPools, 'desktopOnly')}>
+    <table className={clsx(styles.desktopPools, "desktopOnly")}>
       <thead>
-      <tr>
-        <th>Pools</th>
-        <th>APR</th>
-        <th>24H Volume</th>
-        <th>TVL</th>
-        <th/>
-      </tr>
+        <tr>
+          <th>Pools</th>
+          <th>APR</th>
+          <th>24H Volume</th>
+          <th>TVL</th>
+          <th>
+            {" "}
+            <ActionButton
+              className={styles.addButton}
+              onClick={() => {}}
+            >
+              Create a pool
+            </ActionButton>
+          </th>
+          <th />
+        </tr>
       </thead>
       <tbody>
-      {poolsData.map(poolData => {
-        if (!poolData) {
-          return null;
-        }
+        {poolsData.map((poolData) => {
+          if (!poolData) {
+            return null;
+          }
 
-        const { poolId } = poolData;
+          const { poolId } = poolData;
 
-        const key = createPoolKey(poolId);
+          const key = createPoolKey(poolId);
 
-        const coinA = getCoinByAssetId(poolId[0].bits);
-        const coinB = getCoinByAssetId(poolId[1].bits);
+          const coinA = getCoinByAssetId(poolId[0].bits);
+          const coinB = getCoinByAssetId(poolId[1].bits);
 
-        return (
-          <tr key={key}>
-            <td>
-              <CoinPair firstCoin={coinA} secondCoin={coinB} />
-            </td>
-            <td className="blurredText">
-              68,78%
-            </td>
-            <td className="blurredText">
-              $456,567
-            </td>
-            <td className="blurredText">
-              $1,307,567
-            </td>
-            <td>
-              <ActionButton className={styles.addButton} onClick={() => handleAddClick(key)}>
-                Add Liquidity
-              </ActionButton>
-            </td>
-          </tr>
-        );
-      })}
+          return (
+            <tr key={key}>
+              <td>
+                <CoinPair firstCoin={coinA} secondCoin={coinB} />
+              </td>
+              <td className="blurredText">68,78%</td>
+              <td className="blurredText">$456,567</td>
+              <td className="blurredText">$1,307,567</td>
+              <td>
+                <ActionButton
+                  className={styles.addButton}
+                  onClick={() => handleAddClick(key)}
+                >
+                  Add Liquidity
+                </ActionButton>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
