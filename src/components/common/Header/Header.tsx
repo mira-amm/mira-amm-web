585e71fd6a2281a12cd87571f1f28fc7436532f9
Voiislav Mitrovich <voiislav@MacBook-Air-Voiislav.local>
2024-09-24 20:43:01 +0200

    feat: replace points by rewards

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -13,6 +13,7 @@ import DisconnectMobile from "@/src/components/common/ConnectButton/DisconnectMo
 import { useIsConnected } from "@fuels/react";
 import useFaucetLink from "@/src/hooks/useFaucetLink";
 import { BlogLink, TestnetUrl } from "@/src/utils/constants";
+import { RewardsIcon } from "../../icons/Rewards/RewardsIcon";
 
 type Props = {
   isHomePage?: boolean;
@@ -58,16 +59,28 @@ const Header = ({ isHomePage }: Props) => {
             <a href={faucetLink} className={styles.link} target="_blank">
               Faucet
             </a>
-            <div className={styles.pointsText}>
-              Points
-              <SoonLabel className={styles.hiddenLabel} />
-            </div>
+            <a
+              href="https://fuel.mirror.xyz/UfX-NnWGvYy56K8lq5gPfGLPI58DGD3KOHBJupHIibk"
+              className={styles.link}
+              target="_blank"
+            >
+              <div className={styles.rewardsLink}>
+                <RewardsIcon />
+                Rewards
+              </div>
+            </a>
           </div>
         </div>
         <div className={clsx("mobileOnly", styles.links)}>
-          <a className={styles.points}>
-            <span className={styles.pointsText}>Points</span>
-            <SoonLabel />
+          <a
+            href="https://fuel.mirror.xyz/UfX-NnWGvYy56K8lq5gPfGLPI58DGD3KOHBJupHIibk"
+            className={styles.link}
+            target="_blank"
+          >
+            <div className={styles.rewardsLink}>
+              <RewardsIcon />
+              Rewards
+            </div>
           </a>
           <DisconnectMobile className={styles.disconnectMobile} />
           <MobileMenu />
