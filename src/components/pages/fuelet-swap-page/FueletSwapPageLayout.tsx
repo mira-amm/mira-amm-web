f49357e9981da91766b70db7fcc8a2b3daa3ccec
Voiislav Mitrovich <voiislav@MacBook-Air-Voiislav.local>
2024-09-30 21:15:18 +0200

    feat: connect to fuelet from app

--- src/components/pages/fuelet-swap-page/FueletSwapPageLayout.tsx
+++ src/components/pages/fuelet-swap-page/FueletSwapPageLayout.tsx
@@ -5,18 +5,28 @@ import Swap from "../../common/Swap/Swap";
 import styles from "../swap-page/SwapPageLayout.module.css"; // styles for Swap
 import pageStyles from "./FueletSwapPageLayout.module.css"; // custom styles for this page
 import { clsx } from "clsx";
-// import { useRouter } from "next/router";
-// import { useEffect } from "react";
+import { useConnect } from "@fuels/react";
+import { FueletWalletConnector } from "@fuels/connectors";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export const FueletSwapPageLayout = () => {
-//   const router = useRouter();
-//   const { source } = router.query;
+  const router = useRouter();
+  const searchParams = new URLSearchParams(window.location.search);
+  const source = searchParams.get("source");
+  const { connectAsync } = useConnect();
 
-//   useEffect(() => {
-//     if (source !== "fuelet") {
-//       router.push("/404");
-//     }
-//   }, [source, router]);
+  const handleConnect = () => {
+    connectAsync(FueletWalletConnector.name);
+  };
+
+  useEffect(() => {
+    if (source !== "fuelet") {
+      router.push("/404");
+    } else {
+      handleConnect();
+    }
+  }, [source, router]);
 
   return (
     <section className={clsx(pageStyles.page, styles.swapLayout)}>
