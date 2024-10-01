55025e73f9fb1d1b654507b33a58ec500f562425
Nikita Chistousov <svvald@icloud.com>
2024-10-01 19:09:24 +0200

    feat: add transaction parsing (#30)
    * feat: add transaction parsing
    
    * fix: change indexer url to show correct transactions

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,11 +13,12 @@ export const MaxDeadline = 4_294_967_295 as const;
 
 export const DiscordLink = 'https://discord.gg/6pHdTY6rYq' as const;
 export const XLink = 'https://x.com/MiraProtocol' as const;
+export const BlogLink = "https://mirror.xyz/miraly.eth" as const;
 
 export const ValidNetwork = 'testnet' as const;
 export const TestnetUrl = 'https://testnet.fuel.network/v1/graphql' as const;
 
-export const BlogLink = "https://mirror.xyz/miraly.eth" as const;
+export const IndexerUrl = 'https://indexer.bigdevenergy.link/99318f9/v1/graphql' as const;
 
 // TODO: Use env variables for dev/prod
 export const ApiBaseUrl = 'https://prod.api.mira.ly' as const;
