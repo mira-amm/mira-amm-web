734d49c4bb20ef857e5f14edd9b1594ba7fe11fd
mpoplavkov <mihail.poplavkov@gmail.com>
2024-10-03 14:41:31 +0100

    Update contract id

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import type { TxParams } from "fuels";
 
 export const DEFAULT_AMM_CONTRACT_ID =
-  "0xa3925ff18a6a5fb1054ade5e6ee21784fba351ad0e35d38daf8402e1f91779e7" as const;
+  "0x68753c7eba71722d7898fd156d10af32ae9c80d4ee826509fbc07e255d5d985d" as const;
 export const FaucetContractAddress = '0xa1ada1dcab2524dc7f030bbff36c14ede24efd8becffac022a4c501e977e13c6' as const;
 
 export const DefaultTxParams: TxParams = {
