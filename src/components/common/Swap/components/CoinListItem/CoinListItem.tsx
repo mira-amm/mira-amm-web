afcb97e8f50c2fd0d65e1639e577e94a9e4e52b4
Amal Jose <amaljoseami@gmail.com>
2024-12-19 16:33:43 +0530

    Verified flag

--- src/components/common/Swap/components/CoinListItem/CoinListItem.tsx
+++ src/components/common/Swap/components/CoinListItem/CoinListItem.tsx
@@ -2,32 +2,53 @@ import {memo} from "react";
 import {clsx} from "clsx";
 import {BN, CoinQuantity} from "fuels";
 
-import styles from './CoinListItem.module.css';
-import { useAssetImage } from "@/src/hooks/useAssetImage";
+import styles from "./CoinListItem.module.css";
+import {useAssetImage} from "@/src/hooks/useAssetImage";
 import useAssetMetadata from "@/src/hooks/useAssetMetadata";
+import SuccessIcon from "@/src/components/icons/Success/SuccessIcon";
+import {checkIfCoinVerified} from "./checkIfCoinVerified";
+import "react-tooltip/dist/react-tooltip.css";
+import {Tooltip} from "react-tooltip";
 
 type Props = {
   assetId: string;
   balance?: CoinQuantity | undefined;
 };
 
-const CoinListItem = ({ assetId, balance }: Props) => {
+const CoinListItem = ({assetId, balance}: Props) => {
   const metadata = useAssetMetadata(assetId);
   const balanceValue = balance?.amount ?? new BN(0);
   const icon = useAssetImage(assetId);
+  const isVerified = checkIfCoinVerified({
+    symbol: metadata.symbol,
+    assetId: assetId,
+  });
 
   return (
     <span className={clsx(styles.coin, !metadata.name && styles.centered)}>
+      <Tooltip id="verified-tooltip" />
       {icon && <img src={icon} alt={`${metadata.name} icon`} />}
       <div className={styles.names}>
-        <p className={styles.name}>{metadata.symbol}</p>
+        <div className={styles.name_container}>
+          <p className={styles.name}>{metadata.symbol}</p>
+          {isVerified && (
+            <span
+              data-tooltip-id="verified-tooltip"
+              data-tooltip-content="Verified asset from Fuel's official asset list."
+            >
+              <SuccessIcon />
+            </span>
+          )}
+        </div>
         <p className={styles.fullName}>{metadata.name}</p>
       </div>
       {balanceValue.gt(0) && (
-        <p className={styles.balance}>{balanceValue.formatUnits(metadata.decimals || 0)}</p>
+        <p className={styles.balance}>
+          {balanceValue.formatUnits(metadata.decimals || 0)}
+        </p>
       )}
     </span>
-  )
+  );
 };
 
 export default memo(CoinListItem);
