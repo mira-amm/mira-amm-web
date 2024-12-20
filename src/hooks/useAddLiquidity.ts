64cdc5ea0fe1cef85984770e5f23eb7134fc8bcc
Amal Jose <amaljoseami@gmail.com>
2024-12-20 10:25:32 +0530

    Formula fixed

--- src/hooks/useAddLiquidity.ts
+++ src/hooks/useAddLiquidity.ts
@@ -3,8 +3,8 @@ import useMiraDex from "@/src/hooks/useMiraDex/useMiraDex";
 import {useCallback} from "react";
 import {useWallet} from "@fuels/react";
 import {DefaultTxParams, MaxDeadline} from "@/src/utils/constants";
-import {BN} from "fuels";
-import { buildPoolId } from "mira-dex-ts";
+import {bn, BN} from "fuels";
+import {buildPoolId} from "mira-dex-ts";
 
 type Props = {
   firstAsset: string;
@@ -15,9 +15,16 @@ type Props = {
   slippage: number;
 };
 
-const useAddLiquidity = ({ firstAsset, firstAssetAmount, secondAsset, secondAssetAmount, isPoolStable, slippage }: Props) => {
+const useAddLiquidity = ({
+  firstAsset,
+  firstAssetAmount,
+  secondAsset,
+  secondAssetAmount,
+  isPoolStable,
+  slippage,
+}: Props) => {
   const mira = useMiraDex();
-  const { wallet } = useWallet();
+  const {wallet} = useWallet();
 
   const mutationFn = useCallback(async () => {
     if (!mira || !wallet) {
@@ -31,28 +38,55 @@ const useAddLiquidity = ({ firstAsset, firstAssetAmount, secondAsset, secondAsse
     if (poolId[0].bits === firstAsset && poolId[1].bits === secondAsset) {
       asset0Amount = firstAssetAmount;
       asset1Amount = secondAssetAmount;
-    } else if (poolId[0].bits === secondAsset && poolId[1].bits === firstAsset) {
+    } else if (
+      poolId[0].bits === secondAsset &&
+      poolId[1].bits === firstAsset
+    ) {
       asset0Amount = secondAssetAmount;
       asset1Amount = firstAssetAmount;
     } else {
-      throw new Error('Invalid pool id or asset configs');
+      throw new Error("Invalid pool id or asset configs");
     }
 
-    const slippagePercentage = new BN(100 - slippage);
-    const minAsset0Amount = asset0Amount.mul(slippagePercentage).div(100);
-    const minAsset1Amount = asset1Amount.mul(slippagePercentage).div(100);
-    const txRequest = await mira.addLiquidity(poolId, asset0Amount, asset1Amount, minAsset0Amount, minAsset1Amount, MaxDeadline, DefaultTxParams);
+    const minAsset0Amount = asset0Amount
+      .mul(bn(10_000).sub(bn(slippage)))
+      .div(bn(10_000));
+
+    const minAsset1Amount = asset1Amount
+      .mul(bn(10_000).sub(bn(slippage)))
+      .div(bn(10_000));
+
+    const txRequest = await mira.addLiquidity(
+      poolId,
+      asset0Amount,
+      asset1Amount,
+      minAsset0Amount,
+      minAsset1Amount,
+      MaxDeadline,
+      DefaultTxParams
+    );
     const gasCost = await wallet.getTransactionCost(txRequest);
     const fundedTx = await wallet.fund(txRequest, gasCost);
-    const tx = await wallet.sendTransaction(fundedTx, { estimateTxDependencies: true });
+    const tx = await wallet.sendTransaction(fundedTx, {
+      estimateTxDependencies: true,
+    });
     return await tx.waitForResult();
-  }, [mira, wallet, firstAsset, secondAsset, isPoolStable, firstAssetAmount, secondAssetAmount, slippage]);
+  }, [
+    mira,
+    wallet,
+    firstAsset,
+    secondAsset,
+    isPoolStable,
+    firstAssetAmount,
+    secondAssetAmount,
+    slippage,
+  ]);
 
-  const { data, mutateAsync, isPending, error  } = useMutation({
+  const {data, mutateAsync, isPending, error} = useMutation({
     mutationFn,
   });
 
-  return { data, mutateAsync, isPending, error };
+  return {data, mutateAsync, isPending, error};
 };
 
 export default useAddLiquidity;
