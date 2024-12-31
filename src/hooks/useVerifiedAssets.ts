210e93463785f2e4fb0009e799f94a81ca9cf57e
Amal Jose Alex <howareyouami@gmail.com>
2024-12-31 14:45:24 +0530

    Verified flag (#108)
    * Verified flag
    
    * Fetch JSON from server
    
    * Type fixes
    
    * Finish merge
    
    * Setup linting
    
    * Refactor to use useVerifiedAssets hook

---
+++ src/hooks/useVerifiedAssets.ts
@@ -0,0 +1,17 @@
+import {useQuery} from "@tanstack/react-query";
+import {VerifiedAssets} from "../components/common/Swap/components/CoinListItem/checkIfCoinVerified";
+
+export const useVerifiedAssets = () => {
+  const {data: verifiedAssetData} = useQuery({
+    queryKey: ["verifiedAssets"],
+    queryFn: async () => {
+      const req = await fetch(
+        `https://verified-assets.fuel.network/assets.json`
+      );
+      const res = await req.json();
+      return res as VerifiedAssets;
+    },
+  });
+
+  return verifiedAssetData;
+};
