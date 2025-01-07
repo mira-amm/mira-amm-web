9ec2af17190e7e2d770502ddaaad21e111035410
Amal Jose <amaljoseami@gmail.com>
2025-01-07 19:47:52 +0530

    Review before swap

--- src/components/common/Swap/components/SwapFailureModal/SwapFailureModal.tsx
+++ src/components/common/Swap/components/SwapFailureModal/SwapFailureModal.tsx
@@ -1,4 +1,4 @@
-import styles from './SwapFailureModal.module.css';
+import styles from "./SwapFailureModal.module.css";
 import ActionButton from "@/src/components/common/ActionButton/ActionButton";
 import FailureIcon from "@/src/components/icons/Failure/FailureIcon";
 import {ErrorCode, FuelError} from "fuels";
@@ -6,33 +6,35 @@ import {ErrorCode, FuelError} from "fuels";
 type Props = {
   error: Error | null;
   closeModal: VoidFunction;
-}
+  customTitle?: string;
+};
 
-const SwapFailureModal = ({ error, closeModal }: Props) => {
-  let message = 'An error occurred. Please try again.';
-  let title = 'Swap failed';
+const SwapFailureModal = ({error, closeModal, customTitle}: Props) => {
+  let message = "An error occurred. Please try again.";
+  let title = "Swap failed";
 
   if (error instanceof FuelError) {
     message = error.message;
     if (
-      error.code === ErrorCode.SCRIPT_REVERTED && (
-        error.message.includes('Insufficient output amount') || error.message.includes('Exceeding input amount')
-      )
+      error.code === ErrorCode.SCRIPT_REVERTED &&
+      (error.message.includes("Insufficient output amount") ||
+        error.message.includes("Exceeding input amount"))
     ) {
-      message = 'Slippage exceeds limit. Adjust settings and try again.';
+      message = "Slippage exceeds limit. Adjust settings and try again.";
     }
-  } else if (error?.message === 'User rejected the transaction!') {
-    message = 'You closed your wallet before sending the transaction. Try again?';
-    title = 'Swap canceled';
+  } else if (error?.message === "User rejected the transaction!") {
+    message =
+      "You closed your wallet before sending the transaction. Try again?";
+    title = "Swap canceled";
   }
 
   return (
     <div className={styles.claimFailureModal}>
       <FailureIcon />
-      <p className={styles.mainText}>{title}</p>
-      <p className={styles.subText}>
-        {message}
+      <p className={styles.mainText}>
+        {customTitle && customTitle.length > 0 ? customTitle : title}
       </p>
+      <p className={styles.subText}>{message}</p>
       <ActionButton onClick={closeModal} className={styles.viewButton}>
         Try again
       </ActionButton>
