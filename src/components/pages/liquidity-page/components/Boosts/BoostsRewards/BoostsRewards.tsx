ff92f082a0c567826e2c47429931e5800c3a6cec
amaljosea <amaljoseami@gmail.com>
2025-01-07 14:36:57 +0530

    rewards title and learn more button

--- src/components/pages/liquidity-page/components/Boosts/BoostsRewards/BoostsRewards.tsx
+++ src/components/pages/liquidity-page/components/Boosts/BoostsRewards/BoostsRewards.tsx
@@ -1,9 +1,20 @@
 import styles from "./BoostsRewards.module.css";
+import Link from "next/link";
+import ActionButton from "@/src/components/common/ActionButton/ActionButton";
 
 const BoostsRewards = () => {
   return (
-    <div className={styles.rewards}>
+    <div className={styles.boostsHeader}>
       <p className={styles.boostsTitle}>Boost rewards</p>
+      <Link href={`/swap`}>
+        <ActionButton
+          className={styles.learnMoreButton}
+          variant="secondary"
+          fullWidth
+        >
+          Learn more
+        </ActionButton>
+      </Link>
     </div>
   );
 };
