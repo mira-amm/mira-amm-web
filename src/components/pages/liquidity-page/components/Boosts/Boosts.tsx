8666c213f777a5e9bfb1aaa634731f997916e951
amaljosea <amaljoseami@gmail.com>
2025-01-07 14:10:26 +0530

    boosts banner component

--- src/components/pages/liquidity-page/components/Boosts/Boosts.tsx
+++ src/components/pages/liquidity-page/components/Boosts/Boosts.tsx
@@ -1,22 +1,10 @@
-import BoostsIcon from "@/src/components/icons/Boosts/BoostsIcon";
 import styles from "./Boosts.module.css";
+import BoostsBanner from "./BoostsBanner/BoostsBanner";
 
 const Boosts = () => {
   return (
     <section className={styles.boosts}>
-      <div className={styles.banner}>
-        <BoostsIcon />
-        <p className={styles.header}>Introducing Boosts</p>
-        <div className={styles.bottomArea}>
-          <p className={styles.subHeader}>
-            Earn $FUEL by providing liquidity on selected pools. Look for the
-            boost on the pools.
-          </p>
-          <button className={styles.learnMore}>
-            Learn more <span className={styles.arrow}>&rarr;</span>
-          </button>
-        </div>
-      </div>
+      <BoostsBanner />
     </section>
   );
 };
---
+++ src/components/pages/liquidity-page/components/Boosts/BoostsBanner/BoostsBanner.tsx
@@ -0,0 +1,22 @@
+import BoostsIcon from "@/src/components/icons/Boosts/BoostsIcon";
+import styles from "./BoostsBanner.module.css";
+
+const BoostsBanner = () => {
+  return (
+    <div className={styles.banner}>
+      <BoostsIcon />
+      <p className={styles.header}>Introducing Boosts</p>
+      <div className={styles.bottomArea}>
+        <p className={styles.subHeader}>
+          Earn $FUEL by providing liquidity on selected pools. Look for the
+          boost on the pools.
+        </p>
+        <button className={styles.learnMore}>
+          Learn more <span className={styles.arrow}>&rarr;</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BoostsBanner;
