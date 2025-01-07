8b26edb7672bbc3272e245223149485fb4dca3e3
amaljosea <amaljoseami@gmail.com>
2025-01-07 14:16:24 +0530

    boosts rewards component

--- src/components/pages/liquidity-page/components/Boosts/Boosts.tsx
+++ src/components/pages/liquidity-page/components/Boosts/Boosts.tsx
@@ -1,10 +1,14 @@
+import {useIsConnected} from "@fuels/react";
 import styles from "./Boosts.module.css";
 import BoostsBanner from "./BoostsBanner/BoostsBanner";
+import BoostsRewards from "./BoostsRewards/BoostsRewards";
 
 const Boosts = () => {
+  const {isConnected, isPending: isConnecting} = useIsConnected();
+
   return (
     <section className={styles.boosts}>
-      <BoostsBanner />
+      {isConnected ? <BoostsRewards /> : <BoostsBanner />}
     </section>
   );
 };
---
+++ src/components/pages/liquidity-page/components/Boosts/BoostsRewards/BoostsRewards.tsx
@@ -0,0 +1,11 @@
+import styles from "./BoostsRewards.module.css";
+
+const BoostsRewards = () => {
+  return (
+    <div className={styles.rewards}>
+      <p className={styles.boostsTitle}>Boost rewards</p>
+    </div>
+  );
+};
+
+export default BoostsRewards;
