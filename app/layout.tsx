9aca0210bfce0d69a29a0ee6b09bb8ae1e825642
Amal Jose <amaljoseami@gmail.com>
2025-01-13 11:50:31 +0530

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export const metadata: Metadata = {
   title: "Swap on MIRA DEX instantly with low slippage | MIRA Swaps",
   description:
     "Discover seamless crypto swaps with MIRA DEX. Swap your digital assets instantly and securely on the Fuel blockchain. Enjoy best rates and minimal slippage with our optimized trading protocol",
+  icons: {
+    icon: "/images/favicon.png",
+  },
   openGraph: {
     title: "Swap on MIRA DEX instantly with low slippage | MIRA Swaps",
     url: "https://mira.ly/swap",
