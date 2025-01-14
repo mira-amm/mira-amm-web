3e264c3b7310396ff755da96d319341b5fac9a8e
amaljosea <amaljoseami@gmail.com>
2025-01-14 13:00:51 +0530

    get rewards amount hook

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -70,3 +70,4 @@ export const endDate = "2025-03-01T23:59:59"; // March 1, 2025
 export const pairsWithRewards = ["USDC/ETH", "FUEL/ETH", "USDC/USDT"];
 
 export const fuelAmount = 25000;
+export const RewardsApiUrl = "/api/rewards" as const;
