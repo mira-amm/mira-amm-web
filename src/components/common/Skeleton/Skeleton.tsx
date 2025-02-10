ca20d7558cb0a7ab787a0b0d084d148e2e9d3ea4
Vinu <vinucyril@gmail.com>
2025-02-10 15:11:46 +0530

    test commit

--- src/components/common/Skeleton/Skeleton.tsx
+++ src/components/common/Skeleton/Skeleton.tsx
@@ -1,7 +1,7 @@
 import styles from "./Skeleton.module.css";
 import clsx from "clsx";
 
-const Skeleton = ({className}: {className?: string}) => {
+const Skeleton = ({className}: {className?: string}): JSX.Element => {
   return <div className={clsx(styles.skeleton, className)} />;
 };
 
