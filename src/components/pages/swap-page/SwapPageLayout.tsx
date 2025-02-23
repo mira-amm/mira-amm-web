4a55c7f4b95305acc88acc9989535ecb4a6320e7
0xkar4n <karandas388@gmail.com>
2025-02-23 23:21:59 +0530

    fix: removing the non functional back button in swap page

--- src/components/pages/swap-page/SwapPageLayout.tsx
+++ src/components/pages/swap-page/SwapPageLayout.tsx
@@ -3,8 +3,6 @@
 import Header from "@/src/components/common/Header/Header";
 import Footer from "@/src/components/common/Footer/Footer";
 import Swap from "@/src/components/common/Swap/Swap";
-import BackLink from "@/src/components/common/BackLink/BackLink";
-
 import styles from "./SwapPageLayout.module.css";
 
 const SwapPageLayout = () => {
@@ -12,7 +10,6 @@ const SwapPageLayout = () => {
     <div className={styles.swapPage}>
       <Header />
       <main className={styles.swapLayout}>
-        <BackLink chevron />
         <Swap />
       </main>
       <Footer />
