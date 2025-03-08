f91755933a0f530fdd8765870a5e5c908fbfecf1
Mattias Lightstone <87032785+mattiaslightstone@users.noreply.github.com>
2025-03-07 21:50:07 -0500

    Additional style changes

--- src/components/pages/points-page/PointsPageLayout.tsx
+++ src/components/pages/points-page/PointsPageLayout.tsx
@@ -6,27 +6,24 @@ import styles from "./PointsPageLayout.module.css";
 import PointsRankTable from "./PointsRankTable/PointsRankTable";
 import Boosts from "../liquidity-page/components/Boosts/Boosts";
 import pointsStyles from "./PointsStyles.module.css";
+import {POINTS_BANNER_TITLE} from "@/src/utils/constants";
 const PointsPageLayout = (): JSX.Element => {
   return (
     <>
       <Header />
       <main className={styles.pointsPageLayout}>
-        <div className={styles.container}>
-          <Boosts />
+        <Boosts />
+        <h2 className={pointsStyles.pointsTitle}>Leaderboard</h2>
+        <div className={pointsStyles.pointsSubsection}>
+          <p className={pointsStyles.pointsSubtitle}>
+            See the top participants of the points program. Points are updated
+            every hour.
+          </p>
+          <p className={pointsStyles.pointsWarning}>
+            The leaderboard is updated every hour.
+          </p>
         </div>
-        <div className={styles.container}>
-          <h2 className={pointsStyles.pointsTitle}>Leaderboard</h2>
-          <div className={pointsStyles.pointsSubsection}>
-            <p className={pointsStyles.pointsSubtitle}>
-              See the top participants of the points program. Points are updated
-              every hour.
-            </p>
-            <p className={pointsStyles.pointsAlert}>
-              The leaderboard is updated every hour.
-            </p>
-          </div>
-          <PointsRankTable />
-        </div>
+        <PointsRankTable />
       </main>
       <Footer />
     </>
