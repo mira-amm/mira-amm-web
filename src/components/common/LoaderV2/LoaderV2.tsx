cdb523642803e47267c9966d4e5365fc9bf93089
Rishikesh Dasappa <rishikeshdasappa98@gmail.com>
2025-03-13 12:33:10 +0530

    Spinner skeleton scrollbar - color and style update (#7)
    * Skeleton Loader colour update
    
    * LoaderV2 image update
    
    * Scrollbar style update
    
    * removal old spinner image

--- src/components/common/LoaderV2/LoaderV2.tsx
+++ src/components/common/LoaderV2/LoaderV2.tsx
@@ -1,7 +1,7 @@
 import styles from "./LoaderV2.module.css";
 
 const LoaderV2 = () => {
-  return <img src="/images/loader.webp" className={styles.loader} />;
+  return <img src="/images/loader.png" className={styles.loader} />;
 };
 
 export default LoaderV2;
