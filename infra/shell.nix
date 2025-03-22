cec51d02953e4f28f9cd5d34167e330d5c80a5af
Mumtahin Farabi <mfarabi619@gmail.com>
2025-03-21 21:36:50 -0400

    feat(infra/nix): create shell config

---
+++ infra/shell.nix
@@ -0,0 +1,122 @@
+{ pkgs ? import <nixpkgs> {}, DB ? "" }:
+
+let
+  logoPath = "../libs/shared/assets/charthouse-labs-symbol.png";
+  miraLogoPath = "../libs/shared/assets/mira-wordmark-long-light.png";
+  pgLog = "$PGDATA/postgresql.log";
+
+in pkgs.mkShell {
+  name = "mira-dev-env";
+
+  buildInputs = [
+    pkgs.zellij
+    pkgs.neovim
+    pkgs.nerd-fonts.jetbrains-mono
+    pkgs.ascii-image-converter
+    pkgs.postgresql
+    pkgs.lazysql
+    pkgs.lazygit
+    pkgs.btop
+    pkgs.atac
+    pkgs.yazi
+    pkgs.bat
+    pkgs.browsh
+    pkgs.firefox
+    pkgs.zsh
+  ];
+
+  shellHook = ''
+    export PGDATABASE="$DATABASE_NAME"
+    export PGPASSWORD="$DATABASE_PASSWORD"
+    export PGUSER="$DATABASE_USER"
+    export PGHOST="$DATABASE_HOST"
+    export PGPORT="$DATABASE_PORT"
+    export PGDATA="$PWD/pgdata"
+
+    alias psqlx="psql -p $PGPORT -d $PGDATABASE -U $PGUSER"
+
+    start_db() {
+      if [ ! -d "$PGDATA" ]; then
+        echo "Initializing PostgreSQL database..."
+        initdb -D "$PGDATA" --no-locale --encoding=UTF8 --username=$PGUSER
+
+        echo "listen_addresses = '*'" >> "$PGDATA/postgresql.conf"
+        echo "unix_socket_directories = '$PGDATA'" >> "$PGDATA/postgresql.conf"
+
+        echo "host all all 127.0.0.1/32 md5" >> "$PGDATA/pg_hba.conf"
+        echo "local all $PGUSER trust" >> "$PGDATA/pg_hba.conf"
+
+        pg_ctl start -o "-p $PGPORT -k \"$PGDATA\"" -l "${pgLog}"
+
+        psql -d $PGDATABASE -U $PGUSER -c "ALTER USER $PGUSER WITH PASSWORD '$PGPASSWORD';"
+
+        sed -i 's/local all postgres trust/local all postgres md5/' "$PGDATA/pg_hba.conf"
+
+        pg_ctl restart -D "$PGDATA"
+
+        # Create database only if it doesn't exist
+        psql -p $PGPORT -U $PGUSER -tc "SELECT 1 FROM pg_database WHERE datname = '$PGDATABASE'" | grep -q 1 || createdb "$PGDATABASE" -p $PGPORT -U $PGUSER
+      else
+        echo "Starting PostgreSQL..."
+        pg_ctl start -o "-p $PGPORT -k \"$PGDATA\"" -l "${pgLog}"
+      fi
+
+      echo ""
+      echo "***************************************************"
+      echo "PostgreSQL is running on port $PGPORT"
+      echo "DATABASE_URI=$DATABASE_URI"
+      echo "Lazysql Connection URL: postgres://$DATABASE_USER:$DATABASE_PASSWORD@127.0.0.1:$DATABASE_PORT/$DATABASE_NAME/?sslmode=disable"
+      echo "***************************************************"
+      echo ""
+      ascii-image-converter ${logoPath} --color -f -b
+      echo "The Liquidity Hub on Fuel 🟩⛽🔥🐧"
+    }
+
+   stop_db() {
+      echo "Stopping PostgreSQL..."
+      if pg_ctl status -D "$PGDATA" > /dev/null 2>&1; then
+        pg_ctl stop -D "$PGDATA"
+        ascii-image-converter ${miraLogoPath} --color -f -b
+        echo "PostgreSQL stopped. 💤🛌"
+      else
+        ascii-image-converter ${miraLogoPath} --color -f -b
+        echo "PostgreSQL is not running. 💤🛌"
+      fi
+    }
+
+    destroy_db() {
+      echo "Destroying database..."
+      if ls | grep pgdata; then
+        pg_ctl stop -D "$PGDATA"
+        rm -rf $PGDATA
+        echo "PostgreSQL server stopped and all data removed."
+      fi
+    }
+
+    case "${DB}" in
+      up)
+        start_db
+        echo "This pane will close in 3..."
+        sleep 1
+        echo "This pane will close in 2..."
+        sleep 1
+        echo "This pane will close in 1..."
+        sleep 1
+        echo "Happy Hacking! ⌨"
+        exit 0
+        ;;
+      down)
+        stop_db
+        exit 0
+        ;;
+      destroy)
+        destroy_db
+        exit 0
+        ;;
+    esac
+
+stop_db
+echo "🚪 Exiting Nix shell..."
+exit
+ '';
+}
