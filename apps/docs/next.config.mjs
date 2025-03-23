d08bee9201a2a24300aee83c5d20e63bc1d477d7
Mumtahin Farabi <mfarabi619@gmail.com>
2025-03-23 19:12:30 -0400

    feat(ui/docs/header): replace starter logo with Mira brand

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,10 +1,21 @@
 import { createMDX } from 'fumadocs-mdx/next';
 
+const defaultImageHostnames = [
+  'github.com',
+]
+
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: defaultImageHostnames.map(hostname => ({
+      protocol: 'https',
+      // eslint-disable-next-line node/prefer-global/process
+      hostname,
+    })),
+  },
 };
 
 export default withMDX(config);
--- apps/docs/src/app/layout.config.tsx
+++ apps/docs/src/app/layout.config.tsx
@@ -1,21 +1,20 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 import type { HomeLayoutProps } from 'fumadocs-ui/layouts/home'
+import Image from 'next/image'
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <Image src="https://github.com/user-attachments/assets/f4cea652-6234-438b-9b9d-e75cddd4e0c3" alt="Mira Stripes" height={48} width={48} />
+        <span className="text-lg font-bold text-[#01EC97]">
+          Mira
+          {' '}
+          <span className="text-[#F95465]">Docs</span>
+        </span>
       </>
     ),
+    url: '/',
   },
   links: [
     {
