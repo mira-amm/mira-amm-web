06b2d77db5d5719dbc661f475834e76fb504520a
Vinu Cyril <vinucyril@gmail.com>
2025-03-24 14:45:43 +0530

    Liquidity typ fix

--- src/components/pages/add-liquidity-page/components/AddLiquidity/PreviewAddLiquidityDialog.tsx
+++ src/components/pages/add-liquidity-page/components/AddLiquidity/PreviewAddLiquidityDialog.tsx
@@ -139,7 +139,7 @@ const PreviewAddLiquidityDialog = ({
         fullWidth
         size="big"
       >
-        Add Liquidity
+        Add liquidity
       </ActionButton>
       <SuccessModal title={<></>} onClose={redirectToLiquidity}>
         <StatusModal
