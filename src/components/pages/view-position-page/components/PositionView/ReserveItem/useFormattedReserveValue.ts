7391239eadf5240f67ccf519b3b8470d984aebfe
Amal Jose Alex <amaljoseami@gmail.com>
2025-03-25 22:58:46 +0530

    Reward LP view + Add liquidity button (#237)
    * reward lp view + add liquidity button
    
    * codestyle fix
    
    * Fix build failing because of import missing

---
+++ src/components/pages/view-position-page/components/PositionView/ReserveItem/useFormattedReserveValue.ts
@@ -0,0 +1,20 @@
+import {useAssetPriceFromIndexer} from "@/src/hooks/useAssetPriceFromIndexer";
+import {formatDisplayAmount} from "@/src/utils/common";
+
+export const useFormattedReserveValue = (
+  assetId: string,
+  amount: string,
+  reserve: number | undefined,
+) => {
+  const {price} = useAssetPriceFromIndexer(assetId);
+  const usdPrice = parseFloat(price.toFixed(2));
+
+  const valueOfAsset = usdPrice && reserve ? usdPrice * reserve : 0;
+
+  const usdValue = valueOfAsset.toFixed(2);
+
+  return {
+    usdValue,
+    coinAmount: formatDisplayAmount(amount),
+  };
+};
