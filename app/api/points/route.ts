9159e084193ccaa22fe778e2a38781e986c45407
Mattias Lightstone <87032785+mattiaslightstone@users.noreply.github.com>
2025-03-26 00:07:08 -0400

    feat: provide more than 1000 results for points program

--- app/api/points/route.ts
+++ app/api/points/route.ts
@@ -1,7 +1,6 @@
 import {NextRequest, NextResponse} from "next/server";
 import path from "path";
 import {JSONEpochConfigService} from "@/src/models/campaigns/JSONEpochConfigService";
-import {NotFoundError} from "@/src/utils/errors";
 import {TmpFilePointsPerUserService} from "@/src/models/points/Points";
 
 // Cache header settings
