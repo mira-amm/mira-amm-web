cb283cd98ada04035814495e61c9a0abb434fcd5
Mumtahin Farabi <mfarabi619@gmail.com>
2025-03-28 00:14:16 -0400

    feat(ui/docs): change font from inter to prompt

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,15 +1,16 @@
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { Prompt } from 'next/font/google';
 import type { ReactNode } from 'react';
 import '../../../../libs/docs/global.css'
 
-const inter = Inter({
+const prompt = Prompt({
+  weight: ['100','200','300','400','500','600','700','800','900'],
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={prompt.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
