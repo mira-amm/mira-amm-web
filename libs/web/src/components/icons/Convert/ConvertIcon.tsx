85af32612ba22394f8d4bb9fc90cd1d2d8957077
amaljosea <amaljoseami@gmail.com>
2025-03-28 19:08:58 +0530

    Fix: Swap icon jumping

--- libs/web/src/components/icons/Convert/ConvertIcon.tsx
+++ libs/web/src/components/icons/Convert/ConvertIcon.tsx
@@ -1,23 +1,29 @@
 const ConvertIcon = () => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={17}
-      height={17}
-      viewBox="0 0 17 17"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
       fill="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <g
-        stroke="currentColor"
-        strokeLinecap="square"
-        strokeWidth={2}
-        clipPath="url(#a)"
-      >
-        <path d="M1.693 6.138a6.67 6.67 0 0 1 12.723 0m0 0h.305V2.804m-.305 3.334h-3.028M14.416 10.138a6.67 6.67 0 0 1-12.723 0m0 0h-.305v3.333m.305-3.333h3.028" />
+      <g clip-path="url(#clip0_18133_277)">
+        <path
+          d="M1.63867 5.99967C2.48818 3.29494 5.01504 1.33301 8.00014 1.33301C10.9852 1.33301 13.5121 3.29494 14.3616 5.99967M14.3616 5.99967H14.6667V2.66634M14.3616 5.99967H11.3333"
+          stroke="#FDFDFD"
+          stroke-width="2"
+          stroke-linecap="square"
+        />
+        <path
+          d="M14.3615 10.0003C13.512 12.7051 10.9851 14.667 8 14.667C5.01491 14.667 2.48804 12.7051 1.63853 10.0003M1.63853 10.0003H1.33347V13.3337M1.63853 10.0003H4.6668"
+          stroke="#FDFDFD"
+          stroke-width="2"
+          stroke-linecap="square"
+        />
       </g>
       <defs>
-        <clipPath id="a">
-          <path fill="#fff" d="M.055.138h16v16h-16z" />
+        <clipPath id="clip0_18133_277">
+          <rect width="16" height="16" fill="white" />
         </clipPath>
       </defs>
     </svg>
