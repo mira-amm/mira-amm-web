9d2c968f372075259b0611ab15fda7f458d7a6f6
Vinu Cyril <vinucyril@gmail.com>
2025-03-31 14:47:19 +0530

    second phase

--- libs/web/src/components/icons/X/XSocialIcon.tsx
+++ libs/web/src/components/icons/X/XSocialIcon.tsx
@@ -1,6 +1,4 @@
-import {ColorProps} from "../SvgPropTypes";
-
-export const XSocialIcon = ({primaryColor}: ColorProps) => {
+export const XSocialIcon = () => {
   return (
     <svg
       width="20"
@@ -9,7 +7,7 @@ export const XSocialIcon = ({primaryColor}: ColorProps) => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill={primaryColor}
+        fill={"#8A8B8E"}
         d="M18.3528 14.5753L26.5427 5.25977H24.602L17.4906 13.3483L11.8108 5.25977H5.25977L13.8488 17.491L5.25977 27.2598H7.20064L14.7104 18.718L20.7088 27.2598H27.2598L18.3523 14.5753H18.3528ZM15.6945 17.5988L14.8242 16.3808L7.89997 6.68941H10.881L16.469 14.5107L17.3392 15.7287L24.6029 25.8951H21.6218L15.6945 17.5993V17.5988Z"
         fillOpacity="1"
       />
