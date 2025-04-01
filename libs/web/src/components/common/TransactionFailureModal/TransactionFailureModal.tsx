2b5d790076ce15f667c94765f310be7874c6c78f
amaljosea <amaljoseami@gmail.com>
2025-04-01 13:30:37 +0530

    feat: updated typography

--- libs/web/src/components/common/TransactionFailureModal/TransactionFailureModal.tsx
+++ libs/web/src/components/common/TransactionFailureModal/TransactionFailureModal.tsx
@@ -1,6 +1,7 @@
 import styles from "./TransactionFailureModal.module.css";
 import ActionButton from "@/src/components/common/ActionButton/ActionButton";
 import FailureIcon from "@/src/components/icons/Failure/FailureIcon";
+import clsx from "clsx";
 import {FuelError} from "fuels";
 
 type Props = {
@@ -21,8 +22,8 @@ const TransactionFailureModal = ({closeModal, error}: Props) => {
   return (
     <div className={styles.claimFailureModal}>
       <FailureIcon />
-      <p className={styles.mainText}>{title}</p>
-      <p className={styles.subText}>{message}</p>
+      <p className={clsx(styles.mainText, "mc-type-xxl")}>{title}</p>
+      <p className={clsx(styles.subText, "mc-type-b")}>{message}</p>
       <ActionButton onClick={closeModal} fullWidth>
         Try again
       </ActionButton>
