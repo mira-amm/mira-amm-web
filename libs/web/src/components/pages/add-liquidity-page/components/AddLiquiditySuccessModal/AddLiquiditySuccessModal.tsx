ccaf6c4dd3e3a9db7b097d87b34e672a9b5205f3
amaljosea <amaljoseami@gmail.com>
2025-04-01 14:05:58 +0530

    feat: updated typography

--- libs/web/src/components/pages/add-liquidity-page/components/AddLiquiditySuccessModal/AddLiquiditySuccessModal.tsx
+++ libs/web/src/components/pages/add-liquidity-page/components/AddLiquiditySuccessModal/AddLiquiditySuccessModal.tsx
@@ -5,6 +5,7 @@ import {useCallback} from "react";
 import {openNewTab} from "@/src/utils/common";
 import {CoinName} from "@/src/utils/coinsConfig";
 import {FuelAppUrl} from "@/src/utils/constants";
+import clsx from "clsx";
 
 type Props = {
   coinA: CoinName;
@@ -34,8 +35,8 @@ const AddLiquiditySuccessModal = ({
   return (
     <div className={styles.claimFailureModal}>
       <SuccessIcon />
-      <p className={styles.mainText}>Success</p>
-      <p className={styles.subText}>{subText}</p>
+      <p className={clsx(styles.mainText, "mc-type-xxl")}>Success</p>
+      <p className={clsx(styles.subText, "mc-type-b")}>{subText}</p>
       <ActionButton onClick={handleViewTransactionClick} fullWidth>
         View transaction
       </ActionButton>
