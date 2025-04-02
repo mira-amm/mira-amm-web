4fd39850bd4fbbee285cd703e2a7ac49ae42e464
Vinu Cyril <vinucyril@gmail.com>
2025-04-02 11:19:57 +0530

    Loading indicator component

---
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -0,0 +1,7 @@
+import styles from "./LoadingIndicator.module.css";
+
+const LoadingIndicator = (): JSX.Element => {
+  return <span className={styles.loadingAnimation} />;
+};
+
+export default LoadingIndicator;
