9af0c1592ba61787a01f1e96ad60e444b1617891
Rishikesh Dasappa <rishikeshdasappa98@gmail.com>
2025-04-08 23:13:16 +0530

    Logic for loader

--- libs/web/src/components/pages/liquidity-page/components/Positions/Positions.tsx
+++ libs/web/src/components/pages/liquidity-page/components/Positions/Positions.tsx
@@ -3,21 +3,34 @@ import MobilePositions from "@/src/components/pages/liquidity-page/components/Po
 import styles from "./Positions.module.css";
 import DesktopPositions from "@/src/components/pages/liquidity-page/components/Positions/MobilePositions/DesktopPositions/DesktopPositions";
 import usePositions from "@/src/hooks/usePositions";
+import PositionsLoader from "./PositionsLoader/PositionsLoader";
 import DocumentIcon from "@/src/components/icons/Document/DocumentIcon";
-import LoaderV2 from "@/src/components/common/LoaderV2/LoaderV2";
+import { useMemo } from "react";
 
-const Positions = () => {
-  const {data, isLoading} = usePositions();
+const POSITIONS_COUNT = "positions-count";
+
+const Positions = (): JSX.Element => {
+  const { data, isLoading } = usePositions();
+
+  const positionsCount = useMemo(() => {
+    const savedCount = localStorage.getItem(POSITIONS_COUNT);
+    const initialCount = savedCount ? parseInt(savedCount) : 0;
+
+    if (data) {
+      const freshCount = data.length;
+      if (freshCount !== initialCount) {
+        localStorage.setItem(POSITIONS_COUNT, freshCount.toString());
+        return freshCount;
+      }
+    }
+
+    return initialCount;
+  }, [data]);
 
   return (
     <section className={styles.positions}>
       <p className={styles.positionsTitle}>Your Positions</p>
-      {isLoading ? (
-        <div className={styles.positionsFallback}>
-          <LoaderV2 />
-          <p>Loading positions...</p>
-        </div>
-      ) : (data && data.length === 0) || !data ? (
+      {(!data && !isLoading) || data?.length === 0 ? (
         <div className={styles.positionsFallback}>
           <div className={styles.fallbackTop}>
             <div className={styles.icon}>
@@ -25,16 +38,13 @@ const Positions = () => {
             </div>
             <p>Your liquidity will appear here</p>
           </div>
-          {/*<button className={styles.viewArchivedButton}>*/}
-          {/*  View archive positions*/}
-          {/*</button>*/}
         </div>
+      ) : data ? (
+        <DesktopPositions positions={data} />
       ) : (
-        <>
-          <MobilePositions positions={data} />
-          <DesktopPositions positions={data} />
-        </>
+        <PositionsLoader count={positionsCount} />
       )}
+      <MobilePositions positions={data} />
     </section>
   );
 };
