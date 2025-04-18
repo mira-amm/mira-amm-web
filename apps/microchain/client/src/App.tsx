fba31a8fab616bc8efed5fd2cfca7eb5cfb4a729
Mumtahin Farabi <mfarabi619@gmail.com>
2025-04-18 15:36:12 -0400

    feat(microchain): create project

---
+++ apps/microchain/client/src/App.tsx
@@ -0,0 +1,26 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/Home";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <Toaster />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
