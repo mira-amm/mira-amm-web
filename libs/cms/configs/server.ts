b9efe5fcda76e9638a2cede26d719c8346fa7ff0
Mumtahin Farabi <mfarabi619@gmail.com>
2025-04-20 17:17:37 -0400

    feat(infra): migrate db task pipelines to nx

--- libs/cms/configs/server.ts
+++ libs/cms/configs/server.ts
@@ -7,12 +7,12 @@ import sharp from "sharp";
 export const serverConfig = {
 defaultDepth: 3,
   email: resendAdapter({
-    defaultFromAddress: 'test@gmail.com',
-    defaultFromName: 'Mumtahin Farabi',
+    defaultFromAddress: 'test@microchain.systems',
+    defaultFromName: 'Microchain',
     apiKey: process.env.RESEND_API_KEY || '',
   }),
   secret: process.env.PAYLOAD_SECRET,
-  serverURL: process.env.NODE_ENV === 'development' ? process.env.API_LOCAL_URL : process.env.API_PUBLIC_URL,
+  serverURL: process.env.NODE_ENV === 'development' ? process.env.MICROGAME_LOCAL_URL : process.env.MICROGAME_PUBLIC_URL,
   cors: process.env.CORS_WHITELIST_ORIGINS
     ? process.env.CORS_WHITELIST_ORIGINS.split(",")
     : [],
--- libs/db/payload-types.ts
+++ libs/db/payload-types.ts
@@ -190,6 +190,26 @@ export interface Brand {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "media".
+ */
+export interface Media {
+  id: number;
+  alt: string;
+  prefix?: string | null;
+  updatedAt: string;
+  createdAt: string;
+  url?: string | null;
+  thumbnailURL?: string | null;
+  filename?: string | null;
+  mimeType?: string | null;
+  filesize?: number | null;
+  width?: number | null;
+  height?: number | null;
+  focalX?: number | null;
+  focalY?: number | null;
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "forms".
  */
 export interface Form {
