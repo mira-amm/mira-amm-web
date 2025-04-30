0ef6e2d3fa9840cae1aaef0024c6df0398c741b0
Mumtahin Farabi <mfarabi619@gmail.com>
2025-04-30 00:30:27 -0400

    feat(microchain): remove project

--- apps/microchain/client/src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import Terminal from "@/components/Terminal/Terminal";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-black font-['VT323',monospace]">
-      <Terminal />
-    </div>
-  );
-}
