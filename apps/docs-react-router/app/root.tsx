b19890a9c789edca55b5d53820b66ad23af1e0fb
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-06 15:17:25 -0400

    feat(docs-react-router): create project
    feat(fumadocs-react-router): create project

---
+++ apps/docs-react-router/app/root.tsx
@@ -0,0 +1,155 @@
+import type { Route } from './+types/root';
+import {
+  isRouteErrorResponse,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from 'react-router';
+import { RootProvider } from 'fumadocs-ui/provider/base';
+import { ReactRouterProvider } from 'fumadocs-core/framework/react-router';
+import type {BaseLayoutProps} from "fumadocs-ui/layouts/shared";
+import {
+  /* Figma as BrandIcon, */
+  Rss as BlogIcon,
+  Twitter
+} from "lucide-react";
+import {AiOutlineDiscord as DiscordIcon} from "react-icons/ai";
+import './app.css';
+
+export const links: Route.LinksFunction = () => [
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+  {
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossOrigin: 'anonymous',
+  },
+  {
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+  },
+];
+
+export const baseOptions: BaseLayoutProps = {
+  nav: {
+    title: (
+      <>
+        <img
+          src="/mira_stripes.png"
+          alt="Mira Stripes"
+          height={48}
+          width={48}
+        />
+        <span className="text-lg font-bold text-[#01EC97]">
+          Mira <span className="text-[#F95465]">Docs</span>
+        </span>
+      </>
+    ),
+    url: "/",
+  },
+  links: [
+    {
+      text: "Swap",
+      url: "https://mira.ly",
+      external: true,
+    },
+    {
+      text: "Architecture",
+      url: "https://arch.mira.ly",
+      external: true,
+    },
+    {
+      text: "Graph",
+      url: "https://graph.mira.ly",
+      external: true,
+    },
+    {
+      text: "Design",
+      url: "https://design.mira.ly",
+      external: true,
+    },
+    {
+      type: "icon",
+      text: "Blog",
+      url: "https://mirror.xyz/miraly.eth",
+      icon: <BlogIcon />,
+      external: true,
+    },
+    {
+      type: "icon",
+      text: "Discord",
+      url: "https://discord.gg/6pHdTY6rYq",
+      external: true,
+      icon: <DiscordIcon />,
+    },
+    {
+      type: "icon",
+      text: "X (Twitter)",
+      url: "https://x.com/MiraProtocol",
+      icon: <Twitter />,
+      external: true,
+    },
+    /* {
+*   type: "icon",
+*   text: "Brand",
+*   url: "https://figma.com",
+*   external: true,
+*   icon: <BrandIcon />,
+* }, */
+  ],
+  githubUrl: "https://github.com/mira-amm/mira-amm-web",
+};
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <ReactRouterProvider>
+          <RootProvider>{children}</RootProvider>
+        </ReactRouterProvider>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  let message = 'Oops!';
+  let details = 'An unexpected error occurred.';
+  let stack: string | undefined;
+
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? '404' : 'Error';
+    details =
+      error.status === 404
+        ? 'The requested page could not be found.'
+        : error.statusText || details;
+  } else if (import.meta.env.DEV && error && error instanceof Error) {
+    details = error.message;
+    stack = error.stack;
+  }
+
+  return (
+    <main className="pt-16 p-4 container mx-auto">
+      <h1>{message}</h1>
+      <p>{details}</p>
+      {stack && (
+        <pre className="w-full p-4 overflow-x-auto">
+          <code>{stack}</code>
+        </pre>
+      )}
+    </main>
+  );
+}
