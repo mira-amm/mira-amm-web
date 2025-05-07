a5b41ec8cab91cfd4187031ce39baf2e51a5e872
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-05-07 17:45:20 +0100

    chore: remove unused files from swap

--- libs/swap/src/components/pages/create-pool-page/components/CreatePool/CreatePoolTooltips.ts
+++
@@ -1,6 +0,0 @@
-export const VolatilePoolTooltip =
-  "For assets with fluctuating prices. Higher slippage, more flexibility";
-export const StablePoolTooltip =
-  "For assets with stable prices. Lower slippage, less flexibility";
-export const APRTooltip =
-  "Annual return for liquidity providers, including fees and incentives";
