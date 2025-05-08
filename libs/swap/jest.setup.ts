f5ef3821ec5f7c475b591697c4beedee1d093188
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-05-08 12:42:01 +0100

    chore: remove unused files

--- libs/swap/jest.setup.ts
+++ libs/swap/jest.setup.ts
@@ -1,15 +1,15 @@
 // Mock the global fetch API
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    ok: true,
-    json: () => Promise.resolve({}),
-  }),
-) as jest.Mock;
+// global.fetch = jest.fn(() =>
+//   Promise.resolve({
+//     ok: true,
+//     json: () => Promise.resolve({}),
+//   }),
+// ) as jest.Mock;
 
-jest.mock("graphql-request", () => ({
-  request: jest.fn(),
-  gql: jest.fn(),
-}));
+// jest.mock("graphql-request", () => ({
+//   request: jest.fn(),
+//   gql: jest.fn(),
+// }));
 
 // Optionally, suppress console warnings/errors during tests
 // jest.spyOn(console, "error").mockImplementation(() => {});
