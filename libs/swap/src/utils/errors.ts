3090a575813d4f1479ea9d2f59519e3e6b0267be
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-05-08 12:48:44 +0100

    chore: remove unused files

--- libs/swap/src/utils/errors.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Custom error class for handling not found errors
- */
-export class NotFoundError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "NotFoundError";
-  }
-}
--- libs/swap/src/utils/fileLoader.ts
+++
@@ -1,8 +0,0 @@
-/*
- *   File loader to load data once at build time.
- */
-import fs from "fs";
-
-export function loadFile(filepath: string) {
-  return fs.readFileSync(filepath, "utf8");
-}
