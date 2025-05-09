a5c7da948ad2f43e97a24be935e030ae2da75b00
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-05-09 18:17:03 +0100

    chore: add mira domains for bundling

---
+++ libs/swap/src/components/common/Swap/domains.ts
@@ -0,0 +1,15 @@
+export const DOMAINS = [
+  "https://mainnet-explorer.fuel.network",
+  "https://explorer-indexer-mainnet.fuel.network",
+  "https://verified-assets.fuel.network",
+  "https://mira-dex.squids.live/mira-indexer@v3/api/graphql",
+  "https://firebasestorage.googleapis.com",
+  "https://github.com",
+  "https://img.icons8.com",
+  "https://upload.wikimedia.org",
+  "https://12factor.net",
+  "https://firebasestorage.googleapis.com",
+  "https://verified-assets.fuel.network",
+  "https://4021016264-files.gitbook.io",
+  "https://i.imgur.com",
+];
--- libs/swap/src/index.ts
+++ libs/swap/src/index.ts
@@ -1,1 +1,5 @@
+import "./components/common/Swap/Swap.module.css";
+import {DOMAINS} from "./components/common/Swap/domains";
+
 export {default as Swap} from "./components/common/Swap/Swap";
+export {DOMAINS};
