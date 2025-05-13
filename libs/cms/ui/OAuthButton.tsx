4a79e13a0ca204f37a2b6664193dc7de4cd58a61
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-13 03:12:57 -0400

    refactor(microgame,cms): move oauthbutton to lib

---
+++ libs/cms/ui/OAuthButton.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+
+const iconStyle = {
+  marginRight: '8px',
+};
+
+const buttonStyles = {
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: 'none',
+  padding: '12px 0',
+  cursor: 'pointer',
+  color: 'white',
+  fontSize: '16px',
+  fontWeight: 'bold',
+  borderRadius: '5px',
+  transition: 'all 750ms ease-in-out',
+};
+
+export const OAuthButton = ({ href, iconSrc, bgColor, altText }:any) => {
+  return (
+    <Link href={href} style={{ flex: 1 }}>
+      <button style={{ ...buttonStyles, backgroundColor: bgColor }}>
+        <img
+          src={iconSrc}
+          alt={altText}
+          width="24"
+          height="24"
+          style={iconStyle}
+        />
+      </button>
+    </Link>
+  );
+};
