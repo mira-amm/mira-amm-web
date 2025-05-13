ca098180fffd1d630a4dc17c7ef21f289eaddf30
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-13 19:43:02 -0400

    feat(ui/design): update brand image

--- apps/design/.storybook/themes-storybook-ui.ts
+++ apps/design/.storybook/themes-storybook-ui.ts
@@ -81,7 +81,7 @@ export const commonTheme = {
   brandTitle: "Mira",
   brandUrl: "https://mira.ly",
   brandTarget: "_self",
-  brandImage: "https://mira.ly/images/favicon.png",
+  brandImage: "https://axfixdzedlzzumwcftks.supabase.co/storage/v1/object/sign/staging/logo.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InN0b3JhZ2UtdXJsLXNpZ25pbmcta2V5XzA5OTIwOGVlLTRkNmUtNGI4ZC1hN2VjLTcyMDEzOGY1YTEyNCJ9.eyJ1cmwiOiJzdGFnaW5nL2xvZ28uc3ZnIiwiaWF0IjoxNzQ3MTc1MDkyLCJleHAiOjE3Nzg3MTEwOTJ9.Om5ueK1tOOuLP-FyMbSp4-cQb7qa89bEt7FjwptYhkM",
 
   // TYPOGRAPHY
   fontBase: "Jetbrains Mono",
