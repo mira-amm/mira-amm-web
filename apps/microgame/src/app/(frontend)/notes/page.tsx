e6c96c84473911eee8f61838c0287edae97f78ea
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-14 14:11:16 -0400

    feat(microgame/notes): create route

---
+++ apps/microgame/src/app/(frontend)/notes/page.tsx
@@ -0,0 +1,9 @@
+import "@/shared/ui/index.css";
+import SecretNotes from "@/shared/ui/Terminal/SecretNotes";
+
+export default function Notes() {
+
+  return (
+      <SecretNotes onReturn={() =>{} } />
+  );
+}
