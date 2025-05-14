2a42ca63907212cb8d06e9ac4433a08c988b690c
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-14 13:24:31 -0400

    feat(ui/microgame): add wallet connection button to password prompt

--- libs/shared/ui/Terminal/PasswordPrompt.tsx
+++ libs/shared/ui/Terminal/PasswordPrompt.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useState, KeyboardEvent, useRef, useEffect } from 'react';
+import {useConnectUI, useIsConnected} from "@fuels/react";
 
 interface PasswordPromptProps {
   onSubmit: (password: string) => boolean;
@@ -24,6 +25,11 @@ const PasswordPrompt = ({ onSubmit, error }: PasswordPromptProps) => {
       }
     }
   };
+
+  const {connect} = useConnectUI();
+  const {isConnected} = useIsConnected();
+
+  console.log(isConnected)
   
   return (
     <div className="password-prompt mt-6">
@@ -44,8 +50,9 @@ const PasswordPrompt = ({ onSubmit, error }: PasswordPromptProps) => {
       )}
       <p className="text-terminal-text/50 mt-6 text-sm">HINT: The DLM-2000 infrastructure is built on this technology...</p>
 
+      <section className="flex justify-around">
     <Link href="/api/users/oauth/twitter"
-      className="flex bg-black max-w-sm items-center p-4 border border-white rounded-lg my-6 justify-center mx-auto"
+      className="flex bg-black w-sm items-center p-4 border border-white rounded-lg my-6 justify-center mx-auto"
     >
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/5/57/X_logo_2023_%28white%29.png"
@@ -54,6 +61,15 @@ const PasswordPrompt = ({ onSubmit, error }: PasswordPromptProps) => {
           height="24"
         />
     </Link>
+
+        <button className="flex bg-black w-sm space-x-4 items-center p-4 border border-white rounded-lg my-6 justify-center mx-auto hover:cursor-pointer"
+          onClick={connect}
+        >
+          <img className="size-8" src="https://verified-assets.fuel.network/images/fuel.svg"/>
+          <img className="size-8" src="https://avatars.githubusercontent.com/u/178423058?s=48&v=4"/>
+          <img className="size-8" src="https://docs.fuelet.app/~gitbook/image?url=https%3A%2F%2F2435339766-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FT65XO2RJ6uispplU4cJT%252Fuploads%252F3W8rR7UgMOQrtslELoKu%252FFuelet%2520Logo%2520White.svg%3Falt%3Dmedia%26token%3Dd94170ce-168d-4ad8-8c81-5b10ced92afa&width=300&dpr=2&quality=100&sign=cf822ef8&sv=2"/>
+        </button>
+    </section>
     </div>
   );
 };
