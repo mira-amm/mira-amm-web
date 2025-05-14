cd06f6ba5745bfa4290d1e2a6abce0c2d6c2fe62
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-13 22:41:18 -0400

    refactor(microgame/terminal): use nextjs layout

---
+++ apps/microgame/src/app/(frontend)/layout.tsx
@@ -0,0 +1,31 @@
+import {Prompt} from "next/font/google";
+import type {ReactNode} from "react";
+import { ReactNode } from "react";
+import {TerminalHeader} from "@/shared/ui/Terminal/TerminalHeader"
+
+
+const prompt = Prompt({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export default function Layout({children}: {children: ReactNode}) {
+  return (
+    <html lang="en" className={prompt.className} suppressHydrationWarning>
+      <body className="flex h-screen justify-center items-center bg-black font-['VT323',monospace]">
+      {/* Terminal Window */}
+      <div className="terminal-window relative w-full max-w-7xl h-[calc(100vh-20rem)] bg-terminal-bg rounded-md border border-terminal-text/30 overflow-hidden shadow-2xl shadow-terminal-green/20">
+        <TerminalHeader/>
+          {/* Animated scanline effect */}
+        <div className="scanlines relative h-[calc(100%-2rem)] overflow-hidden">
+          <div className="absolute top-0 left-0 w-full h-full bg-terminal-green/5 opacity-30 animate-scanline pointer-events-none z-10"></div>
+        {/* Main Terminal Content */}
+          <main className="h-full overflow-y-auto p-4 text-terminal-text text-lg">
+            {children}
+          </main>
+        </div>
+    </div>
+      </body>
+    </html>
+  );
+}
