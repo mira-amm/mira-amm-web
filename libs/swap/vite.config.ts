68d81ef2a1d492780ae96cfe0f0147a0bbd4ae8d
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-05-20 15:27:35 +0100

    chore(config/swap): change outputPath

--- libs/swap/vite.config.ts
+++ libs/swap/vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: "../../dist/libs/swap",
+    outDir: "../../libs/swap/dist",
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
