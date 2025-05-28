9605a6067cbdd0dd0e3b815d584693309ef00cfa
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-28 00:23:21 -0400

    refactor(web/components/dropdown-menu): use tailwindcss

--- libs/web/src/components/common/DropDownMenu/DropDownMenu.tsx
+++ libs/web/src/components/common/DropDownMenu/DropDownMenu.tsx
@@ -1,8 +1,7 @@
-import React, {forwardRef, MutableRefObject} from "react";
-import styles from "./DropDownMenu.module.css";
+import React, { forwardRef } from "react";
 import clsx from "clsx";
 
-type DropDownMenuProps = {
+ const DropDownMenu = forwardRef<HTMLUListElement, {
   buttons: {
     icon: React.FC;
     text: string;
@@ -11,36 +10,58 @@ type DropDownMenuProps = {
     tooltip?: string;
   }[];
   children?: React.ReactNode;
-};
-
-const DropDownMenu = forwardRef<HTMLUListElement, DropDownMenuProps>(
-  function DropDownMenu({buttons, children}, ref) {
+ }>(
+  function DropDownMenu({ buttons, children }, ref) {
     return (
-      <>
-        <ul className={styles.menuList} ref={ref}>
-          {buttons.map((button) => (
-            <li key={button.text}>
-              <button
+      <ul
+        ref={ref}
+        className={clsx(
+          "absolute top-[50px] right-[-5px] z-20 max-w-[205px] p-[10px] rounded-[12px] bg-[#262834] flex flex-col box-border",
+          "max-[1023px]:fixed max-[1023px]:top-auto max-[1023px]:left-0 max-[1023px]:right-0 max-[1023px]:bottom-0 max-[1023px]:w-full max-[1023px]:max-w-full max-[1023px]:rounded-t-[12px] max-[1023px]:pt-[38px]"
+        )}
+      >
+        {buttons.map((button) => (
+          <li key={button.text} className="relative">
+            <button
+              onClick={button.onClick}
+              disabled={button.disabled}
+              className={clsx(
+                "flex items-center w-full gap-[10px] px-[10px] py-[8px] text-left rounded transition-colors",
+                button.disabled
+                  ? "cursor-default bg-[#262834] text-[var(--content-grey)]"
+                  : "cursor-pointer text-[var(--content-primary)] hover:bg-[var(--background-grey-dark)]",
+              )}
+            >
+              <button.icon />
+              <span
                 className={clsx(
-                  button.disabled
-                    ? styles.menuButtonDisabled
-                    : styles.menuButton,
+                  "font-normal leading-6 whitespace-nowrap",
+                  "text-[15px] max-[1023px]:text-[18px]"
                 )}
-                onClick={button.onClick}
               >
-                <button.icon />
-                <span>{button.text}</span>
-                {button.disabled && button.tooltip && (
-                  <div className={styles.tooltip}>{button.tooltip}</div>
-                )}
-              </button>
-            </li>
-          ))}
-          {children && <div>{children}</div>}
-        </ul>
-      </>
+                {button.text}
+              </span>
+
+              {button.disabled && button.tooltip && (
+                <div
+                  className={clsx(
+                    "absolute z-10 px-2 py-[3px] text-[10px] leading-[14px] font-medium rounded-[20px] whitespace-nowrap",
+                    "bg-[var(--accent-primary)] text-[#292929]",
+                    "left-[-60px] top-1/2 translate-y-[-50%] ml-2",
+                    "max-[1023px]:left-[210px] max-[1023px]:top-[64%] max-[1023px]:visible",
+                    "invisible group-hover:visible"
+                  )}
+                >
+                  {button.tooltip}
+                </div>
+              )}
+            </button>
+          </li>
+        ))}
+        {children && <div>{children}</div>}
+      </ul>
     );
-  },
+  }
 );
 
 export default DropDownMenu;
