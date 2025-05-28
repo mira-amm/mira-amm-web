4b973359611c4ebd78e60cfa93ce3027524fe6b5
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-27 23:57:00 -0400

    refactor(web/components/coin): use tailwindcss

--- libs/web/src/components/common/Coin/Coin.tsx
+++ libs/web/src/components/common/Coin/Coin.tsx
@@ -1,41 +1,37 @@
-import {memo} from "react";
-
-import styles from "./Coin.module.css";
-import {clsx} from "clsx";
+import { clsx } from "clsx";
 import ChevronDownIcon from "@/src/components/icons/ChevronDownIcon";
 import useAssetMetadata from "@/src/hooks/useAssetMetadata";
-import {useAssetImage} from "@/src/hooks/useAssetImage";
+import { useAssetImage } from "@/src/hooks/useAssetImage";
 
-type Props = {
+export default function Coin({ assetId, className, onClick }: {
   assetId: string | null;
   className?: string;
   onClick?: VoidFunction;
-};
-
-const Coin = ({assetId, className, onClick}: Props) => {
+}){
   const metadata = useAssetMetadata(assetId);
   const icon = useAssetImage(assetId);
 
   const handleClick = () => {
-    if (onClick) {
-      onClick();
-    }
+    if (onClick) onClick();
   };
 
   const newPool = Boolean(onClick);
 
+  const baseClasses = "flex items-center gap-2 text-[var(--content-primary)]";
+  const clickableClasses =
+    "rounded px-2 py-1 cursor-pointer hover:bg-[var(--background-grey-light)] active:bg-[var(--background-grey-dark)]";
+  const nameClasses = "text-[18px] font-medium leading-[19px]";
+
   return (
     <div
-      className={clsx(styles.coin, newPool && styles.clickable)}
+      className={clsx(baseClasses, newPool && clickableClasses)}
       onClick={handleClick}
     >
       {icon && <img src={icon} alt={`${metadata.symbol} icon`} />}
-      <p className={clsx(styles.name, className)}>
+      <p className={clsx(nameClasses, className)}>
         {metadata.symbol ?? "Choose Asset"}
       </p>
       {newPool && <ChevronDownIcon />}
     </div>
   );
 };
-
-export default Coin;
