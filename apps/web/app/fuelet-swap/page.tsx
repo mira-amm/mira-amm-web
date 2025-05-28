be5b8b4b8a950de68384aa60b39c3c840b42558b
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-28 00:42:02 -0400

    refactor(web/fuelet-swap): use next.js layout & tailwindcss

--- apps/web/app/fuelet-swap/page.tsx
+++ apps/web/app/fuelet-swap/page.tsx
@@ -1,7 +1,41 @@
-import {FueletSwapPageLayout} from "@/src/components/pages/fuelet-swap-page/FueletSwapPageLayout";
+"use client";
 
-const FueletSwap = () => {
-  return <FueletSwapPageLayout />;
+import BackLink from "@/src/components/common/BackLink/BackLink";
+import Swap from "@/src/components/common/Swap/Swap";
+import {useConnect} from "@fuels/react";
+import {FueletWalletConnector} from "@fuels/connectors";
+// import { useRouter } from "next/navigation";
+import {useEffect} from "react";
+
+export default function Page(){
+  // const router = useRouter();
+  // const searchParams = new URLSearchParams(window.location.search);
+  // const source = searchParams.get("source");
+  const {connectAsync} = useConnect();
+
+  const handleConnect = () => {
+    connectAsync(FueletWalletConnector.name);
+  };
+
+  useEffect(() => {
+    // if (source !== "fuelet") {
+    //   router.push("/404");
+    // } else {
+    handleConnect();
+    // }
+  }, []);
+
+  return (
+    <>
+      <BackLink
+        title="Powered by mira.ly"
+        href="https://mira.ly"
+      />
+    <div className="flex flex-1 flex-col items-center w-full md:justify-center">
+      <div className="w-full max-w-lg px-4">
+        <Swap />
+      </div>
+    </div>
+    </>
+  );
 };
-
-export default FueletSwap;
