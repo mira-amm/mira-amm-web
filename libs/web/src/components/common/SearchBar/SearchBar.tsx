ace73012b6c4d6f6406aedba3f1ac77c3e6cc4e7
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-28 00:20:25 -0400

    refactor(web/components/search-bar): use tailwindcss

--- libs/web/src/components/common/SearchBar/SearchBar.tsx
+++ libs/web/src/components/common/SearchBar/SearchBar.tsx
@@ -1,25 +1,29 @@
-import {clsx} from "clsx";
-import styles from "./SearchBar.module.css";
+import { clsx } from "clsx";
 import SearchIcon from "../../icons/SearchIcon";
-import {SearchBarProps} from "./SearchBarProps";
+import { RefObject, ChangeEvent } from "react";
 
-export const SearchBar: React.FC<SearchBarProps> = ({
-  placeholder,
-  onChange,
-  inputRef,
-  className,
-  value,
-}) => {
+export const SearchBar: React.FC<{
+  placeholder: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  inputRef?: RefObject<HTMLInputElement>;
+  className?: string;
+  value: string;
+}> = ({ placeholder, onChange, inputRef, className, value }) => {
   return (
-    <div className={clsx(styles.searchBar, className)}>
+    <div
+      className={clsx(
+        "flex gap-[10px] p-[14px_12px] rounded-lg text-[var(--content-grey)] bg-[var(--background-grey-dark)]",
+        className
+      )}
+    >
       <SearchIcon />
       <input
-        className={styles.searchInput}
         type="text"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
         ref={inputRef}
+        className="flex-1 text-base bg-transparent border-none outline-none text-[var(--content-primary)] placeholder:text-[var(--content-grey)]"
       />
     </div>
   );
