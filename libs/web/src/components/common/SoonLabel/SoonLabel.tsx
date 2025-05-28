1d7534e4e5c13add4b55131571d9c54400176084
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-28 00:20:05 -0400

    refactor(web/components/soon-label): use tailwindcss

--- libs/web/src/components/common/SoonLabel/SoonLabel.tsx
+++ libs/web/src/components/common/SoonLabel/SoonLabel.tsx
@@ -1,14 +1,19 @@
-import {memo} from "react";
+import { memo } from "react";
+import { clsx } from "clsx";
 
-import styles from "./SoonLabel.module.css";
-import {clsx} from "clsx";
-
-type Props = {
+const SoonLabel = ({ className }: {
   className?: string;
-};
-
-const SoonLabel = ({className}: Props) => {
-  return <span className={clsx(styles.soonLabel, className)}>Soon</span>;
+}) => {
+  return (
+    <span
+      className={clsx(
+        "px-2 py-[2px] rounded-[20px] font-medium text-[10px] leading-[14px] text-[var(--background-primary)] bg-[var(--accent-primary)] lowercase",
+        className
+      )}
+    >
+      Soon
+    </span>
+  );
 };
 
 export default memo(SoonLabel);
