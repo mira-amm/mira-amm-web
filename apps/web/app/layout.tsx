79a2e72d5f221c1511a77abd05e1cf2ac326293f
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-29 11:03:12 -0400

    perf(web): remove google script

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -74,13 +74,6 @@ export default function Layout({children}: Props){
           </div>
         </Providers>
       </body>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-K113JNM8XN" />
-      <Script id="gtag">{`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){ dataLayer.push(arguments); }
-      gtag('js', new Date());
-      gtag('config', 'G-K113JNM8XN');
-    `}</Script>
     </html>
   );
 };
