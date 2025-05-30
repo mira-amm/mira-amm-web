43f996b1af050482f5b44fa32d5b4216a41dfe89
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-29 20:52:02 -0400

    perf(web/api/latest-block): slow response due unneccessary string conversions

--- apps/web/app/api/latest-block/route.ts
+++ apps/web/app/api/latest-block/route.ts
@@ -1,12 +1,11 @@
 /**
  * @api {get} /latest-block Get latest block data (only if events data available for the block)
  */
-// library
-import {NextRequest, NextResponse} from "next/server";
-import {request, gql} from "graphql-request";
-import {DateTime} from "fuels";
 
-// local imports
+import { NextRequest, NextResponse } from "next/server";
+import { request, gql } from "graphql-request";
+import { DateTime } from "fuels";
+
 import {
   SQDIndexerUrl,
   NetworkUrl,
@@ -17,10 +16,9 @@ import {
   SQDIndexerResponses,
 } from "@/web/shared/types";
 
-// Function to fetch the squid status (height)
-async function fetchSquidStatus(
+const fetchSquidStatus = async (
   url: string,
-): Promise<SQDIndexerResponses.SquidStatus> {
+): Promise<SQDIndexerResponses.SquidStatus> => {
   const query = gql`
     query GetSquidStatus {
       squidStatus {
@@ -29,26 +27,18 @@ async function fetchSquidStatus(
     }
   `;
 
-  try {
-    const response = await request<{
-      squidStatus: SQDIndexerResponses.SquidStatus;
-    }>({
-      url,
-      document: query,
-    });
+  const { squidStatus } = await request<{ squidStatus: SQDIndexerResponses.SquidStatus }>({
+    url,
+    document: query,
+  });
 
-    return response.squidStatus;
-  } catch (error) {
-    console.error("Error fetching squid status:", error);
-    throw error;
-  }
-}
+  return squidStatus;
+};
 
-// Function to fetch the block data by height (timestamp)
-async function fetchBlockByHeight(
+const fetchBlockByHeight = async (
   url: string,
   height: string,
-): Promise<FuelAPIResponses.BlockByHeight> {
+): Promise<FuelAPIResponses.BlockByHeight> => {
   const query = gql`
     query GetBlockByHeight($height: String!) {
       block(height: $height) {
@@ -59,58 +49,36 @@ async function fetchBlockByHeight(
     }
   `;
 
-  try {
-    const response = await request<{block: FuelAPIResponses.BlockByHeight}>({
-      url,
-      document: query,
-      variables: {height},
-    });
+  const { block } = await request<{ block: FuelAPIResponses.BlockByHeight }>({
+    url,
+    document: query,
+    variables: { height },
+  });
 
-    return response.block;
-  } catch (error) {
-    console.error("Error fetching block by height:", error);
-    throw error;
-  }
-}
+  return block;
+};
 
-// Handle GET requests for /api/latest-block
 export async function GET(req: NextRequest) {
   try {
-    // Fetch the squid status to get the current height (latest block)
-    const squidStatus = await fetchSquidStatus(SQDIndexerUrl);
+    const { finalizedHeight: blockNumber } = await fetchSquidStatus(SQDIndexerUrl);
 
-    const blockNumber = squidStatus.finalizedHeight; // Latest block number
+    const { header } = await fetchBlockByHeight(NetworkUrl, blockNumber.toString());
 
-    // Fetch the block data (timestamp) using the block number
-    const blockData = await fetchBlockByHeight(
-      NetworkUrl,
-      blockNumber.toString(),
-    );
+    const blockTimestamp = DateTime.fromTai64(header.time.toString()).toUnixSeconds();
 
-    const tai64TimeString = blockData.header.time.toString();
-
-    // converting TAI64 string to normal datetime
-    const blockDateTime: DateTime = DateTime.fromTai64(tai64TimeString);
-    const blockTimeStampInSeconds = blockDateTime.toUnixSeconds();
-
-    // Combine block number (height) and block timestamp
     const block: GeckoTerminalQueryResponses.Block = {
       blockNumber,
-      blockTimestamp: blockTimeStampInSeconds,
+      blockTimestamp,
     };
 
-    const latestBlockResponse: GeckoTerminalQueryResponses.LatestBlockResponse =
-      {
-        block,
-      };
+    const latestBlockResponse: GeckoTerminalQueryResponses.LatestBlockResponse = { block };
 
-    // Return the block data
     return NextResponse.json(latestBlockResponse);
   } catch (error) {
     console.error("error fetching latest block:", error);
     return NextResponse.json(
-      {error: "An unexpected error occurred while fetching latest block"},
-      {status: 500},
+      { error: "An unexpected error occurred while fetching latest block" },
+      { status: 500 },
     );
   }
 }
