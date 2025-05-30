9c63aa2c7bda4c75e71e01b8c2efae569c8dc906
Mumtahin Farabi <mfarabi619@gmail.com>
2025-05-29 20:56:00 -0400

    refactor(web/api/pair): use early returns

--- apps/web/app/api/pair/route.ts
+++ apps/web/app/api/pair/route.ts
@@ -1,30 +1,24 @@
 /**
  * @api {get} /pair Get pair details by id
  */
-import {NextRequest, NextResponse} from "next/server";
-import {request, gql} from "graphql-request";
-import {SQDIndexerUrl} from "../../../../../libs/web/src/utils/constants";
+import { NextRequest, NextResponse } from "next/server";
+import { request, gql } from "graphql-request";
+import { SQDIndexerUrl } from "../../../../../libs/web/src/utils/constants";
 import {
   SQDIndexerResponses,
   GeckoTerminalQueryResponses,
 } from "../../../../../libs/web/shared/types";
-import {NotFoundError} from "../../../../../libs/web/src/utils/errors";
+import { NotFoundError } from "../../../../../libs/web/src/utils/errors";
 
-// Function to fetch pool details by ID
-async function fetchPoolById(
-  poolId: string,
-): Promise<SQDIndexerResponses.Pool> {
-  // Define the GraphQL query to fetch pool details by the given id
+const fetchPoolById = async (
+  poolId: string
+): Promise<SQDIndexerResponses.Pool> => {
   const query = gql`
     query GetPoolById($id: String!) {
       poolById(id: $id) {
         id
-        asset0 {
-          id
-        }
-        asset1 {
-          id
-        }
+        asset0 { id }
+        asset1 { id }
         creationBlock
         creationTime
         creationTx
@@ -32,73 +26,59 @@ async function fetchPoolById(
     }
   `;
 
-  // Send the GraphQL request to the indexer server to fetch the pool data
-  const response = await request<{
-    poolById: SQDIndexerResponses.Pool;
-  }>({
+  const { poolById } = await request<{ poolById: SQDIndexerResponses.Pool }>({
     url: SQDIndexerUrl,
     document: query,
-    variables: {id: poolId},
+    variables: { id: poolId },
   });
 
-  // throw error if pool is not found
-  if (!response.poolById) {
+  if (!poolById) {
     throw new NotFoundError(`Pool with ID: ${poolId} not found`);
   }
 
-  // return pool/pair data
-  return response.poolById;
-}
+  return poolById;
+};
 
-function createPairFromPool(
-  pool: SQDIndexerResponses.Pool,
-): GeckoTerminalQueryResponses.Pair {
-  const pair: GeckoTerminalQueryResponses.Pair = {
-    id: pool.id,
-    dexKey: "mira",
-    asset0Id: pool.asset0.id,
-    asset1Id: pool.asset1.id,
-    createdAtBlockNumber: pool.creationBlock,
-    createdAtBlockTimestamp: pool.creationTime,
-    createdAtTxnId: pool.creationTx,
-  };
+const createPairFromPool = (
+  pool: SQDIndexerResponses.Pool
+): GeckoTerminalQueryResponses.Pair => ({
+  id: pool.id,
+  dexKey: "mira",
+  asset0Id: pool.asset0.id,
+  asset1Id: pool.asset1.id,
+  createdAtBlockNumber: pool.creationBlock,
+  createdAtBlockTimestamp: pool.creationTime,
+  createdAtTxnId: pool.creationTx,
+});
 
-  return pair;
-}
-
-// Handle GET requests for /api/pair
 export async function GET(req: NextRequest) {
   try {
-    const url = new URL(req.url);
-    const poolId = url.searchParams.get("id");
+    const poolId = new URL(req.url).searchParams.get("id");
 
-    // Return a 400 error if no 'id' is provided
     if (!poolId) {
       return NextResponse.json(
-        {error: "Pool ID(param: id) is required"},
-        {status: 400},
+        { error: "Pool ID(param: id) is required" },
+        { status: 400 }
       );
     }
 
-    // Fetch the pool details
     const pool = await fetchPoolById(poolId);
-    // Format pool data (pair and pool are synonymous) according to Gecko spec
-    const pair = createPairFromPool(pool);
-    const pairResponse: GeckoTerminalQueryResponses.PairResponse = {
-      pair,
-    };
-    // Return the fetched pool(pair) data
+
+    const pair: GeckoTerminalQueryResponses.Pair = createPairFromPool(pool);
+
+    const pairResponse: GeckoTerminalQueryResponses.PairResponse = { pair };
+
     return NextResponse.json(pairResponse);
   } catch (error) {
     console.error("Error fetching pair data:", error);
 
     if (error instanceof NotFoundError) {
-      return NextResponse.json({error: error.message}, {status: 404});
+      return NextResponse.json({ error: error.message }, { status: 404 });
     }
 
     return NextResponse.json(
-      {error: "An unexpected error occurred while fetching pair data"},
-      {status: 500},
+      { error: "An unexpected error occurred while fetching pair data" },
+      { status: 500 }
     );
   }
 }
