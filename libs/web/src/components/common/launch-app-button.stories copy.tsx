a7135f688832ece5e2098c303c47c3c5db486e09
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-05-30 22:08:27 +0100

    feat(web/launch-app-button): add storybook documentation

---
+++ libs/web/src/components/common/launch-app-button.stories copy.tsx
@@ -0,0 +1,28 @@
+import type {Meta, StoryObj} from "@storybook/react";
+import "../../../styles.css";
+import {LaunchAppButton} from "./launch-app-button";
+
+const meta = {
+  title: "🪙 Web/Launch App Button",
+  component: LaunchAppButton,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [
+    (Story) => (
+      <div className="bg-[var(--background-primary)] p-4">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof LaunchAppButton>;
+
+export default meta;
+type Story = StoryObj<typeof LaunchAppButton>;
+
+export const Default: Story = {
+  args: {
+    route: [""],
+  },
+};
