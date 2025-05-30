b040a21580c16b11267975524644fdb0eacfe43c
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-05-30 22:18:42 +0100

    refactor(libs/web/steps-block): remove font-medium

--- libs/web/src/components/common/steps-block.tsx
+++ libs/web/src/components/common/steps-block.tsx
@@ -11,7 +11,7 @@ export const StepsBlock: React.FC<{
   return (
     <div className="bg-[#262934] rounded-2xl w-[350px] flex flex-col items-center gap-4 px-6 pt-7 pb-10 box-border lg:w-full lg:pb-8">
       {logo}
-      <h3 className="font-medium text-[26px] leading-8 text-center mt-[22px] font-[var(--font-inter)] lg:text-[24px] lg:leading-[30px]">
+      <h3 className="text-[26px] leading-8 text-center mt-[22px] font-[var(--font-inter)] lg:text-[24px] lg:leading-[30px]">
         {title}
       </h3>
       <p className="font-normal text-[16px] leading-6 text-center text-[var(--content-dimmed-light)]">
