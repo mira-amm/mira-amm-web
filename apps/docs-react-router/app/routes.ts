211d340607645b89dd50d2ee6a8d52d84bc91efc
Mumtahin Farabi <mfarabi619@gmail.com>
2025-06-08 16:21:09 -0400

    feat(docs-react-router): delete project

--- apps/docs-react-router/app/routes.ts
+++
@@ -1,8 +0,0 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes';
-
-export default [
-  // TODO: Render introduction page as '/' route
-  index('routes/home.tsx'),
-  route('/*', 'docs/page.tsx'),
-  route('api/search', 'docs/search.ts'),
-] satisfies RouteConfig;
