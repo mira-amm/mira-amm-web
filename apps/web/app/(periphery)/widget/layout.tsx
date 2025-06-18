c9aee63fc76c5e1b0f561cfc85ca8a42e89888a1
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-06-18 19:06:06 +0100

    fix(widget): use dark mode

--- apps/web/app/(periphery)/widget/layout.tsx
+++ apps/web/app/(periphery)/widget/layout.tsx
@@ -20,7 +20,7 @@ const inter = Inter({
 
 export default function Layout({children}: {readonly children: ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <link rel="preload" as="image" href="/images/loader.webp" />
       </head>
--- apps/web/app/(periphery)/widget/page.tsx
+++ apps/web/app/(periphery)/widget/page.tsx
@@ -2,7 +2,7 @@ import Swap from "@/src/components/common/Swap/Swap";
 
 export default function Page() {
   return (
-    <div className="flex w-full max-w-lg mx-auto">
+    <div className="flex justify-center items-center w-full h-full max-w-lg mx-auto">
       <Swap isWidget />
     </div>
   );
