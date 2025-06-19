02a281fa73cfd1d63acaa91a67b2fa73f4c78181
Mumtahin Farabi <mfarabi619@gmail.com>
2025-06-19 08:33:29 -0400

    refactor(web/components/swap): consolidate imports

--- libs/web/src/components/common/Swap/Swap.tsx
+++ libs/web/src/components/common/Swap/Swap.tsx
@@ -1,6 +1,29 @@
 "use client";
 
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  memo
+} from "react";
+
+import {ArrowUpDown, LoaderCircle} from "lucide-react";
+
+import {clsx} from "clsx";
+
+import {
+  B256Address,
+  bn,
+  ScriptTransactionRequest,
+  TransactionCost,
+} from "fuels";
+
 import {useConnectUI, useIsConnected} from "@fuels/react";
+
+import {PoolId} from "mira-dex-ts";
+
 import {
   CoinsListModal,
   SwapSuccessModal,
@@ -9,13 +32,22 @@ import {
   Loader,
   SlippageSetting,
   FeatureGuard,
+  PriceImpact,
+  ConnectWallet,
+  triggerClassAnimation
 } from "@/src/components/common";
-import {useCallback, useEffect, useMemo, useRef, useState, memo} from "react";
+
+import SwapFailureModal from "@/src/components/common/Swap/components/SwapFailureModal/SwapFailureModal";
+
+import {
+  createPoolKey,
+  openNewTab
+} from "@/src/utils/common";
+
 import CurrencyBox from "@/src/components/common/Swap/components/CurrencyBox/CurrencyBox";
 import ExchangeRate from "@/src/components/common/Swap/components/ExchangeRate/ExchangeRate";
-import useExchangeRate from "@/src/hooks/useExchangeRate/useExchangeRate";
-import {createPoolKey, openNewTab} from "@/src/utils/common";
 import SettingsModalContent from "@/src/components/common/Swap/components/SettingsModalContent/SettingsModalContent";
+import useExchangeRate from "@/src/hooks/useExchangeRate/useExchangeRate";
 import useInitialSwapState from "@/src/hooks/useInitialSwapState/useInitialSwapState";
 import useCheckActiveNetwork from "@/src/hooks/useCheckActiveNetwork";
 import usePreview from "@/src/hooks/useSwapPreviewV2";
@@ -25,14 +57,9 @@ import {
 } from "@/src/components/common/Swap/components/price-impact";
 import {FuelAppUrl} from "@/src/utils/constants";
 import useReservesPrice from "@/src/hooks/useReservesPrice";
-import SwapFailureModal from "@/src/components/common/Swap/components/SwapFailureModal/SwapFailureModal";
-import {
-  B256Address,
-  bn,
-  ScriptTransactionRequest,
-  TransactionCost,
-} from "fuels";
-import {PoolId} from "mira-dex-ts";
+
+import {FuelAppUrl} from "@/src/utils/constants";
+
 import {
   useIsClient,
   useAssetMetadata,
@@ -42,11 +69,10 @@ import {
   useSwap,
   useAssetImage,
   useAssetPrice,
+  TradeState
 } from "@/src/hooks";
-import {TradeState} from "@/src/hooks/useSwapRouter";
+
 import {useAnimationStore} from "@/src/stores/useGlitchScavengerHunt";
-import {triggerClassAnimation} from "../GlitchEffects/ClassAnimationTrigger";
-import {ConnectWallet} from "../connect-wallet";
 import {Button} from "@/meshwave-ui/Button";
 import {ArrowUpDown, LoaderCircle} from "lucide-react";
 import {cn} from "@/src/utils/cn";
