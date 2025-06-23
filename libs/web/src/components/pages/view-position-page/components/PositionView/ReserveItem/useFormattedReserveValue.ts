d1e2aadac3b925beed25ccff2aae9a1e882b5e6b
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-06-23 19:47:49 +0100

    refactor(web/reserve-item): fix dollar value display

--- libs/web/src/components/pages/view-position-page/components/PositionView/ReserveItem/useFormattedReserveValue.ts
+++ libs/web/src/components/pages/view-position-page/components/PositionView/ReserveItem/useFormattedReserveValue.ts
@@ -1,17 +1,17 @@
 import {useAssetPriceFromIndexer} from "@/src/hooks/useAssetPriceFromIndexer";
 import {formatDisplayAmount} from "@/src/utils/common";
+import {formatMoney} from "@/src/utils/formatMoney";
 
 export const useFormattedReserveValue = (
   assetId: string,
   amount: string,
   reserve: number | undefined,
 ) => {
-  const {price} = useAssetPriceFromIndexer(assetId);
-  const usdPrice = parseFloat(price.toFixed(2));
+  const {price: usdPrice} = useAssetPriceFromIndexer(assetId);
 
-  const valueOfAsset = usdPrice && reserve ? usdPrice * reserve : 0;
+  const valueOfAsset = reserve ? usdPrice * reserve : 0;
 
-  const usdValue = valueOfAsset.toFixed(2);
+  const usdValue = formatMoney(valueOfAsset);
 
   return {
     usdValue,
