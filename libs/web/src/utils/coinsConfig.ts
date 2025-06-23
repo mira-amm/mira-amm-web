6e18af9ee3a713741e220fd2996be91452b7d5a4
Mumtahin Farabi <mfarabi619@gmail.com>
2025-06-23 17:40:34 -0400

    feat(ui/web/swap): change initial assets from eth and usdc to fuel and eth

--- libs/web/src/utils/coinsConfig.ts
+++ libs/web/src/utils/coinsConfig.ts
@@ -139,10 +139,10 @@ export const BASE_ASSETS: CoinData[] = [
       "0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07",
   },
   {
-    name: "USDC",
-    symbol: "USDC",
-    decimals: 6,
+    name: "Fuel",
+    symbol: "FUEL",
+    decimals: 9,
     assetId:
-      "0x286c479da40dc953bddc3bb4c453b608bba2e0ac483b077bd475174115395e6b",
+      "0x1d5d97005e41cae2187a895fd8eab0506111e0e2f3331cd3912c15c24e3c1d82",
   },
 ];
