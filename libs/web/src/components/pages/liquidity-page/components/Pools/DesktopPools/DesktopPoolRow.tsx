9ddc103aa8fe751357eec4812a5f38b277ed7c63
Mattias Lightstone <87032785+mattiaslightstone@users.noreply.github.com>
2025-06-25 19:03:48 -0400

    style(web): Remove top margin in liquidity pools apr cell

--- libs/web/src/components/pages/liquidity-page/components/Pools/DesktopPools/DesktopPoolRow.tsx
+++ libs/web/src/components/pages/liquidity-page/components/Pools/DesktopPools/DesktopPoolRow.tsx
@@ -1,22 +1,22 @@
-import {usePoolDetails} from "../usePoolDetails";
+import { usePoolDetails } from "../usePoolDetails";
 import CoinPair from "@/src/components/common/CoinPair/CoinPair";
 import Link from "next/link";
 import clsx from "clsx";
-import {PoolData} from "@/src/hooks/usePoolsData";
+import { PoolData } from "@/src/hooks/usePoolsData";
 import AprBadge from "@/src/components/common/AprBadge/AprBadge";
 import usePoolNameAndMatch from "@/src/hooks/usePoolNameAndMatch";
-import {Button} from "@/meshwave-ui/Button";
-import {TableCell, TableRow} from "@/meshwave-ui/table";
+import { Button } from "@/meshwave-ui/Button";
+import { TableCell, TableRow } from "@/meshwave-ui/table";
 
 const cellBase =
   "px-6 py-4 whitespace-nowrap overflow-hidden text-ellipsis text-center";
 
-const DesktopPoolRow = ({poolData}: {poolData: PoolData}) => {
-  const {poolKey, aprValue, volumeValue, tvlValue, isStablePool, poolId} =
+const DesktopPoolRow = ({ poolData }: { poolData: PoolData }) => {
+  const { poolKey, aprValue, volumeValue, tvlValue, isStablePool, poolId } =
     usePoolDetails(poolData);
 
   const tvlActual = parseInt(tvlValue?.replace(/[^0-9]+/g, ""), 10);
-  const {isMatching} = usePoolNameAndMatch(poolKey);
+  const { isMatching } = usePoolNameAndMatch(poolKey);
 
   return (
     <TableRow key={poolKey}>
@@ -32,7 +32,7 @@ const DesktopPoolRow = ({poolData}: {poolData: PoolData}) => {
       <TableCell
         className={clsx(
           cellBase,
-          "overflow-visible mt-[26px] flex justify-center items-center",
+          "overflow-visible flex justify-center items-center",
           !isMatching && !aprValue && "text-content-dimmed-light",
         )}
       >
