cffa8150af49328b2f8843420284855757565363
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-07-03 14:41:38 +0100

    fix(web): enforce only dark mode

--- apps/web/app/(core)/layout.tsx
+++ apps/web/app/(core)/layout.tsx
@@ -30,7 +30,12 @@ export default function Layout({children}: {readonly children: ReactNode}) {
   const glitchScavengerHuntEnabled = useAnimationStore.getState().masterEnabled;
 
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html
+      lang="en"
+      className="dark"
+      suppressHydrationWarning
+      style={{colorScheme: "dark"}}
+    >
       <head>
         <link rel="preload" as="image" href="/images/loader.webp" />
       </head>
