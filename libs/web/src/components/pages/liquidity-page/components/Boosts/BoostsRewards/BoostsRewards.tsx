65940a840dca5f9ab80f9140934679182ae3555a
Brendan Lucas <brendanwflucas@gmail.com>
2025-07-21 15:40:10 -0700

    fix points background gradient

--- libs/web/src/components/pages/liquidity-page/components/Boosts/BoostsRewards/BoostsRewards.tsx
+++ libs/web/src/components/pages/liquidity-page/components/Boosts/BoostsRewards/BoostsRewards.tsx
@@ -29,7 +29,7 @@ export function BoostsRewards() {
   }
 
   return (
-    <div className="flex flex-col gap-6 max-[480px]:gap-4">
+    <div className="flex flex-col gap-6 max-[480px]:gap-4 ">
       <div className="flex justify-between items-center">
         <p className="text-2xl">Points Program</p>
         <Link href={POINTS_LEARN_MORE_URL} target="_blank">
@@ -39,7 +39,7 @@ export function BoostsRewards() {
 
       <div
         className={clsx(
-          "flex justify-between items-center gap-4 rounded-[10px] min-h-[110px] bg-background-primary dark:bg-[linear-gradient(170deg,#262f5f_35%,#c41cff_100%)] p-6"
+          "flex justify-between items-center gap-4 rounded-[10px] min-h-[110px] bg-[url('/images/pointsGradientBackground.png')] p-6"
         )}
       >
         <div className="flex flex-col gap-2">
