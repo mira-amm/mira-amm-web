f65594ac3c5419a47e6bffd37e8d72f077282864
Brendan Lucas <brendanwflucas@gmail.com>
2025-08-04 16:26:05 -0700

    remove promotion points card, move ticker tape top above navbar

--- apps/web/app/(core)/page.tsx
+++ apps/web/app/(core)/page.tsx
@@ -8,7 +8,6 @@ import {AnimatedText} from "@/src/components/common/AnimatedText/AnimatedText";
 import {AnimatePresence} from "framer-motion";
 import {RebrandInfoCard} from "@/src/components/common/rebrand-info-card";
 import {TickerTape} from "@/src/components/common/ticker-tape-banner";
-import {TickerTapeText} from "@/src/utils/constants";
 
 export default function Page() {
   const {isConnected} = useIsConnected();
@@ -25,9 +24,7 @@ export default function Page() {
     <div className="flex flex-1 flex-col items-center w-full gap-10 lg:justify-center lg:min-h-[calc(100vh-120px)]">
       {isConnected ? (
         <div className="h-10"></div>
-      ) : (
-        <TickerTape text={TickerTapeText} />
-      )}
+      ) : (<></>)}
       <div className="w-full max-w-lg px-4 relative">
         <section className="space-y-10">
           <AnimatedText
