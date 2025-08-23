6ee7134bf2393c0c13fca312b5e1270603d98172
Mumtahin Farabi <mfarabi619@gmail.com>
2025-08-23 11:32:38 -0600

    perf(microvisor/cachix): activate binary cache
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- apps/microvisor/cachix/default.nix
+++ apps/microvisor/cachix/default.nix
@@ -2,13 +2,19 @@
   cachix = {
     enable = true;
     pull = [
-      "fuellabs"      # fuellabs.cachix.org-1:3gOmll82VDbT7EggylzOVJ6dr0jgPVU/KMN6+Kf8qx8=
+      "fuellabs"
       "pre-commit-hooks"
       # "rad"
-      "oxalica"       # oxalica.cachix.org-1:h0iRBw6tQD8+51ZvnNEBPbwLR58UD7klauDBWzBdugQ=
-      "nixpkgs"       # nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE=
-      "nix-community" # nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+      "oxalica"
+      "nixpkgs"
+      "nix-community"
+      "devenv"
+      "nix-darwin"
+      "mfarabi"
+      "cachix"
+      "emacs-ci"
     ];
+    push = "charthouse-labs";
   };
 }
 
