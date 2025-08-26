7acd4b03f7f35604e91d0c528a9ac6351ebb2548
Mattias Lightstone <87032785+mattiaslightstone@users.noreply.github.com>
2025-08-25 22:36:27 -0400

    feat(ts-sdk): Add utility functions for mira-v2

--- libs/ts-sdk/src/sdk/index.ts
+++ libs/ts-sdk/src/sdk/index.ts
@@ -1,6 +1,7 @@
 export * from "./mira_amm";
 export * from "./readonly_mira_amm";
 export * from "./mira_amm_v2";
+export * from "./readonly_mira_amm_v2";
 export * from "./model";
 export * from "./errors";
 export * from "./cache";
@@ -11,4 +12,20 @@ export {
   buildPoolIdV2,
   getLPAssetIdV2,
   poolIdV2Input,
+  reorderPoolIdV2,
+  extractAssetsFromPoolIdV2,
+  arrangePoolParamsV2,
+  poolInputFromAssets,
+  poolContainsAssetV2,
+  binIdToDelta,
+  deltaToBinId,
+  binIdToPrice,
+  priceToBinId,
+  calculateLiquidityDistribution,
+  calculateBinAmounts,
+  getBinRange,
+  getActiveBinRange,
+  validateBinStep,
+  validateBaseFactor,
+  calculateBinPriceRange,
 } from "./utils";
