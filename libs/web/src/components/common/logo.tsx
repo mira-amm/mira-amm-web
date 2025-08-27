fe5509f528d2e3123314f457744fcdd0ec4d4492
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-08-27 17:33:11 +0100

    feat(web/common): remove components with new suffix

--- libs/web/src/components/common/logo.tsx
+++ libs/web/src/components/common/logo.tsx
@@ -1,5 +1,12 @@
-import {LogoNew} from "./logo-new";
+import Link from "next/link";
+import LogoIconFooter from "./Logo/LogoIconFooter";
+import LogoIcon from "./Logo/LogoIcon";
+import {cn} from "@/src/utils/cn";
 
 export function Logo({isFooter = false}: {isFooter?: boolean}) {
-  return <LogoNew isFooter={isFooter} />;
+  return (
+    <Link href="/" className={cn(!isFooter && "w-[150px]", "dark:text-white")}>
+      {isFooter ? <LogoIconFooter /> : <LogoIcon />}
+    </Link>
+  );
 }
