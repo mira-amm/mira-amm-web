4f185972a5cc0027f34e6d6d1b086fb12617c7c4
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-09-18 23:48:25 +0100

    fix: mir-457 responsive liquidity shape selector

--- libs/web/src/components/pages/bin-liquidity/components/liquidity-shape.tsx
+++ libs/web/src/components/pages/bin-liquidity/components/liquidity-shape.tsx
@@ -75,7 +75,7 @@ const LiquidityShapeSelector = ({
   setLiquidityShape: Dispatch<React.SetStateAction<LiquidityShape>>;
 }) => {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <LiquidityShapeOption
         shape="spot"
         label="Spot"
