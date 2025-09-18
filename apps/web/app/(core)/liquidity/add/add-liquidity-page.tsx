f14f8c13a07c0ea06c4bdaa479ecd98c7e676864
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-09-18 23:32:30 +0100

    fix: mir-457 increase headline font-size one level up 16px to 18px

--- apps/web/app/(core)/liquidity/add/add-liquidity-page.tsx
+++ apps/web/app/(core)/liquidity/add/add-liquidity-page.tsx
@@ -63,7 +63,7 @@ export default function AddLiquidityPage() {
       </button>
       <section className="flex flex-col p-4 rounded-ten gap-6 bg-background-grey-dark border-border-secondary border-[12px] dark:border-0 dark:bg-background-grey-dark z-[5] w-full max-w-[563px] mx-auto">
         <div className="flex items-center w-full pb-4 border-b border-background-grey-light gap-2.5  text-sm leading-[19px] text-content-grey">
-          <p className="flex-1 text-content-primary text-base ">
+          <p className="flex-1 text-content-primary text-lg">
             Add Liquidity
           </p>
           <SlippageSetting
