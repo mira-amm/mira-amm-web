9e6608b947d96ce4fe64e60d1153fdcd28daf540
Mattias Lightstone <87032785+mattiaslightstone@users.noreply.github.com>
2025-10-02 20:26:28 -0400

    test(ts-sdk/v2): Add vitest config and scripts for running sdk integration tests

--- libs/ts-sdk/vitest.config.ts
+++ libs/ts-sdk/vitest.config.ts
@@ -10,7 +10,10 @@ export default defineConfig({
     watch: false,
     globals: true,
     environment: "node",
-    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    include: [
+      "src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+      "test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+    ],
     reporters: ["default"],
     coverage: {
       reportsDirectory: "../../coverage/libs/mira-v1-ts",
