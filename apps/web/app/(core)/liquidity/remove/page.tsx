32704c345df6b1262b239d8d8966f59ddee75c70
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2025-10-24 14:30:22 +0100

    refactor: use pool router to route to the correct page

--- apps/web/app/(core)/liquidity/remove/page.tsx
+++ apps/web/app/(core)/liquidity/remove/page.tsx
@@ -1,7 +1,6 @@
-import {Suspense} from "react";
-import {Loader} from "@/src/components/common";
-import RemoveLiquidityWithRouting from "./remove-liquidity-with-routing";
+import {PageSuspense, PoolRouter} from "@/src/components/common";
 import RemoveLiquidityPage from "./remove-liquidity-page";
+import V2RemoveLiquidityPage from "./v2-remove-liquidity-page";
 
 type PageProps = {
   searchParams: Promise<{[key: string]: string | undefined}>;
@@ -10,20 +9,18 @@ type PageProps = {
 export default async function Page({searchParams}: PageProps) {
   const {pool} = await searchParams;
 
-  const render = () => {
-    if (!pool) return <RemoveLiquidityPage />;
-    return <RemoveLiquidityWithRouting poolKey={pool} />;
-  };
-
   return (
-    <Suspense
-      fallback={
-        <div className="flex justify-center items-center">
-          <Loader color="gray" rebrand={true} />
-        </div>
-      }
-    >
-      {render()}
-    </Suspense>
+    <PageSuspense>
+      <PoolRouter
+        pool={pool}
+        renderV1={({poolKey}) => <RemoveLiquidityPage poolKey={poolKey} />}
+        renderV2={({poolKey, unifiedPoolIdString}) => (
+          <V2RemoveLiquidityPage
+            poolKey={poolKey}
+            unifiedPoolIdString={unifiedPoolIdString}
+          />
+        )}
+      />
+    </PageSuspense>
   );
 }
